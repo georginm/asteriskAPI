177d4641f8b2c277cc0f89f8f356bd612bdb5653
georginmatheus <georginmatheus@gmail.com>
2021-05-26 22:28:26 -0300

    alterando a estrutura do arquivo

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require('express'); //importando o objeto
+const routes = require('./routes') //o ./ é para mostrar que é um arquivo e não um pacote
+const app = express(); //instanciando o objeto
+const cors = require('cors')
 
-class App{
-    constructor(){
-        this.server = express();
-        this.routes();
-    }
+// app.use(cors())
 
-    routes() {
-        this.server.use(routes);
-    }
-}
+//informa para o express que será usado a estrutura JSON
+app.use(express.json());
+app.use(routes)
 
-module.exports = new App().server;
+
+module.exports = app;
 
