9417e24e82dcf3f8f0c7f7d6e0d4d8a053f57963
georginmatheus <georginmatheus@gmail.com>
2021-05-27 15:19:36 -0300

    Adicionado rotas do CRUD de inserçãode membros na fila e rotas das filas

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const queuesControllers = require('./controllers/queuesControllers');
+const queueMemberControllers = require('./controllers/queueMemberControllers');
 
 const routes = new Router();
 
@@ -8,9 +9,15 @@ routes.get('/', (req,res) => {
 });
 
 routes.get('/queues', queuesControllers.index);
-routes.post('/queues/store', queuesControllers.store);
+routes.get('/queues/list', queuesControllers.list);
+routes.post('/queues/create', queuesControllers.create);
 routes.delete('/queues/:name', queuesControllers.delete);
-routes.put('/queue/:name', queuesControllers.update);
+routes.put('/queues/:name', queuesControllers.update);
 
+
+routes.get('/queuemembers', queueMemberControllers.index);
+routes.post('/queuemembers/create', queueMemberControllers.create);
+routes.put('/queuemembers/:protocol/:ramal', queueMemberControllers.update);
+routes.delete('/queuemembers/:protocol/:ramal', queueMemberControllers.delete);
 
 module.exports = routes;
