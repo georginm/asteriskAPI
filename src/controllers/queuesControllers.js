8f6ca214c51405604790460a45b2d69a47d7d336
georginmatheus <georginmatheus@gmail.com>
2021-05-29 22:29:29 -0300

    corrigindo bug da função create

--- src/controllers/queuesControllers.js
+++ src/controllers/queuesControllers.js
@@ -33,15 +33,11 @@ module.exports = {
     },
 
     async create(req, res){
-        // Eu não sei ainda qual a primary key da tabela, suspeito que seja o nome
-        // enquanto n verifico deixarei essa verificação abaixo
-        const { name } = req.body
         try {
-            const query = await db.select('queues', "name", {name});
+            const query = await db.select('queues', "name", {name: req.body.name});
             // Essa interface já está em algum registro do banco?
             if(query.length == 0) { // Caso não tenha registro
-                const lastId = await db.getLastId('queues');
-                const query = await db.insert('queues', {id: lastId, ...req.body}, select);
+                const query = await db.insert('queues', req.body, select);
                 return res.status(200).json(query);
             }
             else { //Caso tenha
@@ -82,16 +78,14 @@ module.exports = {
                     query = await db.update('queues',req.body, {name: req.params.name});
                     return res.status(204).send();
                 } else {
-                    query = await db.select("queues", "name", req.body.name);
+                    query = await db.select("queues", "name", {name: req.body.name});
                     if(query.length == 0){
                         query = await db.update('queues',req.body, {name: req.params.name});
                         return res.status(204).send();
                     } else {
                         return res.status(401).json({error: "O nome fornecido no body já pertence a outra fila."});
                     }
-                        
                 }
-                
             }
         } catch (error) {
             return res.status(400).json({error: `${error.message}`})
