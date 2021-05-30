de525db71b483eb0d4ed3c8d8f4046591a03975e
georginmatheus <georginmatheus@gmail.com>
2021-05-29 21:00:23 -0300

    Inserção das rotas dos endpoints, aors, auth

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 const { Router } = require('express');
+
 const queuesControllers = require('./controllers/queuesControllers');
 const queueMemberControllers = require('./controllers/queueMemberControllers');
 const ramalControllers = require('./controllers/ramalControllers');
+const aorsControllers = require('./controllers/aorsControllers');
+const endpointsController = require('./controllers/endpointsController');
+const authControllers = require('./controllers/authControllers');
 
 const routes = new Router();
 
-routes.get('/', (req,res) => {
-    res.send('Hello World');
-});
+routes.get('/', ramalControllers.index);
+routes.get('/ramais', ramalControllers.list);
+
 
 routes.get('/queues', queuesControllers.index);
 routes.get('/queues/list', queuesControllers.list);
@@ -23,5 +27,23 @@ routes.delete('/queuemembers/:protocol/:ramal', queueMemberControllers.delete);
 
 routes.get('/ramais', ramalControllers.index);
 routes.get('/ramais/list', ramalControllers.list);
+
+routes.get('/aors', aorsControllers.index);
+routes.get('/aors/list', aorsControllers.list);
+routes.post('/aors/create', aorsControllers.create);
+routes.delete('/aors/:id', aorsControllers.delete);
+routes.put('/aors/:id', aorsControllers.update);
+
+routes.get('/endpoint', endpointsController.index);
+routes.get('/endpoint/list', endpointsController.list);
+routes.post('/endpoint/create', endpointsController.create);
+routes.put('/endpoint/:id', endpointsController.update);
+routes.delete('/endpoint/:id', endpointsController.delete);
+
+routes.get('/auth', authControllers.index);
+routes.get('/auth/list', authControllers.list);
+routes.post('/auth/create', authControllers.create);
+routes.delete('/auth/:id', authControllers.delete);
+routes.put('/auth/:id', authControllers.update);
 
 module.exports = routes;
