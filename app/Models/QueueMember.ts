1cce99a2bde264b7c14b8291851116c0f7d3570f
georginmatheus <georginmatheus@gmail.com>
2021-06-29 15:45:44 -0300

    feat: add queue_member model

---
+++ app/Models/QueueMember.ts
@@ -0,0 +1,31 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class QueueMember extends BaseModel {
+  public static table = 'queue_members'
+  @column({ isPrimary: true })
+  public id: number
+
+  @column({ columnName: 'queue_name' })
+  public queueName: string
+
+  @column()
+  public interface: string
+
+  @column({ columnName: 'membername' })
+  public memberName: string
+
+  @column({ columnName: 'state_interface' })
+  public stateInterface: string
+
+  @column()
+  public penalty: number
+
+  @column()
+  public paused: number
+
+  @column({ columnName: 'uniqueid' })
+  public uniqueId: number
+
+  @column({ columnName: 'wrapuptime' })
+  public wrapUpTime: number
+}
