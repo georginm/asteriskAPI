a76b4705d3f33d85dac515bd70e36e238cae9eaf
georginmatheus <georginmatheus@gmail.com>
2021-06-30 16:08:28 -0300

    feat: add iax controller and model

---
+++ app/Controllers/Http/IaxsController.ts
@@ -0,0 +1,70 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { badRequest, success } from 'App/Helpers/http-helper'
+import Iax from 'App/Models/Iax'
+import Endpoint from 'App/Models/Endpoint'
+
+export default class IaxsController {
+  public async index({ response }: HttpContextContract) {
+    const data = await Iax.all()
+    return success(response, data)
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const { id } = request.body()
+
+    const dataExists = await Iax.find(id)
+
+    if (dataExists) {
+      return badRequest(response, { message: 'Iax Already Exists' })
+    }
+
+    const data = await Iax.create(request.body())
+
+    return success(response, data, 201)
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    const { id } = request.params()
+
+    const data = await Iax.find(id)
+
+    if (!data) {
+      return badRequest(response, { message: 'Iax Not Exists' }, 404)
+    }
+
+    data.merge(request.body())
+
+    await data.save()
+
+    return success(response, data)
+  }
+
+  public async delete({ request, response }: HttpContextContract) {
+    const { id } = request.params()
+
+    const data = await Iax.find(id)
+
+    if (!data) {
+      return badRequest(response, { message: 'Iax Not Exists' }, 404)
+    }
+
+    await data.delete()
+
+    const endpoint = await Endpoint.find(id)
+    if (endpoint) {
+      await endpoint.delete
+    }
+
+    return success(response, { message: 'Iax Has Been Deleted' })
+  }
+
+  public async list({ request, response }: HttpContextContract) {
+    const { id } = request.params()
+    const data = await Iax.find(id)
+    if (!data) {
+      return badRequest(response, { message: 'Iax Not Exists' })
+    }
+
+    return success(response, data, 200)
+  }
+}
---
+++ app/Models/Iax.ts
@@ -0,0 +1,63 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Iax extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public type: string
+
+  @column({ columnName: 'username' })
+  public userName: string
+
+  @column()
+  public secret: string
+
+  @column()
+  public context: string
+
+  @column()
+  public ipaddr: string
+
+  @column()
+  public port: string
+
+  @column({ columnName: 'regseconds' })
+  public regSeconds: string
+
+  @column()
+  public host: string
+
+  @column({ columnName: 'defaultip' })
+  public defaultIp: string
+
+  @column()
+  public mask: string
+
+  @column({ columnName: 'callerid' })
+  public callerId: string
+
+  @column()
+  public trunk: string
+
+  @column()
+  public auth: string
+
+  @column({ columnName: 'maxauthreq' })
+  public maxAuthReq: number
+
+  @column()
+  public encryption: string
+
+  @column()
+  public transfer: string
+
+  @column()
+  public disallow: string
+
+  @column()
+  public allow: string
+}
