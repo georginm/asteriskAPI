194c41efe7eed7257332f391059bb2f4dfd6a018
georginmatheus <georginmatheus@gmail.com>
2021-07-01 08:57:20 -0300

    refactor: edit http response

--- app/Helpers/http-helper.ts
+++ app/Helpers/http-helper.ts
@@ -1,21 +1,17 @@
 import { ResponseContract } from '@ioc:Adonis/Core/Response'
 
-export const success = (response: ResponseContract, data: object, statusCode = 200): void => {
-  response.status(statusCode).send(data)
+export const ok = (response: ResponseContract, data: object): void => {
+  response.status(200).send(data)
 }
 
-export const successCreated = (
-  response: ResponseContract,
-  data: object,
-  statusCode = 201
-): void => {
-  response.status(statusCode).send(data)
+export const created = (response: ResponseContract, data: object): void => {
+  response.status(201).send(data)
 }
 
-export const badRequest = (response: ResponseContract, data: object, statusCode = 400): void => {
-  response.status(statusCode).send(data)
+export const badRequest = (response: ResponseContract, data: string): void => {
+  response.status(400).send({ message: data })
 }
 
-export const notFound = (response: ResponseContract, data: object, statusCode = 404): void => {
-  response.status(statusCode).send(data)
+export const notFound = (response: ResponseContract, data: string): void => {
+  response.status(404).send({ message: data })
 }
