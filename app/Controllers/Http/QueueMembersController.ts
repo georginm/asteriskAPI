22d69a99057730bf482d6a998908c5cb9c031abc
georginmatheus <georginmatheus@gmail.com>
2021-07-02 15:05:43 -0300

    refactor: remove semicolon

--- app/Controllers/Http/QueueMembersController.ts
+++ app/Controllers/Http/QueueMembersController.ts
@@ -1,89 +1,85 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import {
-  badRequest,
-  created,
-  success,
-} from 'App/Helpers/http-helper';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { badRequest, created, success } from 'App/Helpers/http-helper'
 
-import QueueMember from 'App/Models/QueueMember';
-import Queue from 'App/Models/Queue';
-import { alreadyExists, insert } from 'App/Services/DatabaseMethods';
+import QueueMember from 'App/Models/QueueMember'
+import Queue from 'App/Models/Queue'
+import { alreadyExists, insert } from 'App/Services/DatabaseMethods'
 
 export default class QueueMembersController {
   public async index({ response }: HttpContextContract) {
-    const data = await QueueMember.query().orderBy('interface');
-    return success(response, data);
+    const data = await QueueMember.query().orderBy('interface')
+    return success(response, data)
   }
 
   public async store({ request, response }: HttpContextContract) {
-    const inter = request.only(['interface']);
-    const queue = request.only(['queue_name']);
+    const inter = request.only(['interface'])
+    const queue = request.only(['queue_name'])
 
-    const queueAlreadyExists = await Queue.find(queue);
+    const queueAlreadyExists = await Queue.find(queue)
 
     if (!queueAlreadyExists) {
-      return badRequest(response, 'Queue Not Found');
+      return badRequest(response, 'Queue Not Found')
     }
 
-    const dataExists = await alreadyExists('queue_members', inter);
+    const dataExists = await alreadyExists('queue_members', inter)
 
     if (dataExists) {
-      return badRequest(response, 'QueueMember Already Exists');
+      return badRequest(response, 'QueueMember Already Exists')
     }
 
-    const data = await insert('queue_members', request.body());
+    const data = await insert('queue_members', request.body())
 
-    return created(response, data);
+    return created(response, data)
   }
 
   public async update({ request, response }: HttpContextContract) {
-    const { inter } = request.params();
+    const { inter } = request.params()
 
-    const data = await QueueMember.find(inter);
+    const data = await QueueMember.find(inter)
 
     if (!data) {
-      return badRequest(response, 'QueueMember Not Exists');
+      return badRequest(response, 'QueueMember Not Exists')
     }
 
-    data.merge(request.body());
+    data.merge(request.body())
 
-    await data.save();
+    await data.save()
 
-    return success(response, data);
+    return success(response, data)
   }
 
   public async destroy({ request, response }: HttpContextContract) {
-    const { protocol, endpoint } = request.params();
+    const { protocol, endpoint } = request.params()
 
     const data = await QueueMember.findBy(
       'interface',
       `${protocol + '/' + endpoint}`
-    );
+    )
 
     // const dataExists = await alreadyExists('queue_members', inter)
     if (!data) {
-      return badRequest(response, 'QueueMember Not Exists');
+      return badRequest(response, 'QueueMember Not Exists')
     }
 
     // console.log(data.$attributes)
-    await QueueMember.query().where(data.$attributes).delete();
+    await QueueMember.query().where(data.$attributes).delete()
 
     return success(response, {
       message: 'QueueMember Has Been Deleted',
-    });
+    })
   }
 
   public async list({ request, response }: HttpContextContract) {
-    const { protocol, endpoint } = request.params();
+    const { protocol, endpoint } = request.params()
 
     const data = await QueueMember.findBy(
       'interface',
       `${protocol + '/' + endpoint}`
-    );
+    )
     if (!data) {
-      return badRequest(response, 'QueueMember Not Exists');
+      return badRequest(response, 'QueueMember Not Exists')
     }
 
-    return success(response, data);
+    return success(response, data)
   }
 }
--- app/Helpers/http-helper.ts
+++ app/Helpers/http-helper.ts
@@ -1,29 +1,29 @@
-import { ResponseContract } from '@ioc:Adonis/Core/Response';
+import { ResponseContract } from '@ioc:Adonis/Core/Response'
 
 export const success = (
   response: ResponseContract,
   data: object
 ): void => {
-  response.status(200).send(data);
-};
+  response.status(200).send(data)
+}
 
 export const created = (
   response: ResponseContract,
   data: object
 ): void => {
-  response.status(201).send(data);
-};
+  response.status(201).send(data)
+}
 
 export const badRequest = (
   response: ResponseContract,
   data: string
 ): void => {
-  response.status(400).send({ message: data });
-};
+  response.status(400).send({ message: data })
+}
 
-// export const notFound = (
-//   response: ResponseContract,
-//   data: string
-// ): void => {
-//   response.status(404).send({ message: data });
-// };
+export const notFound = (
+  response: ResponseContract,
+  data: string
+): void => {
+  response.status(404).send({ message: data })
+}
