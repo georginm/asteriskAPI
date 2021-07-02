673890a2bbc9c05f8cdf411e2d0eb3aa24c94721
georginmatheus <georginmatheus@gmail.com>
2021-07-02 15:06:26 -0300

    refactor: remove semicolon and add not found get route

--- start/routes.ts
+++ start/routes.ts
@@ -1,73 +1,78 @@
-import Route from '@ioc:Adonis/Core/Route';
+import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hello: 'world' };
-});
+  return { hello: 'world' }
+})
 
 Route.group(() => {
   // Endpoint Routes
   Route.resource('/endpoints', 'EndpointsController')
     .except(['create', 'show', 'edit'])
-    .as('endpoint');
+    .as('endpoint')
 
   // Auth Routes
   Route.resource('auths', 'AuthsController')
     .except(['create', 'show', 'edit'])
-    .as('auths');
+    .as('auths')
 
   // Aor Routes
   Route.resource('aors', 'AorsController')
     .except(['create', 'show', 'edit'])
-    .as('aors');
+    .as('aors')
 
   // Extension Routes
   Route.resource('extensions', 'ExtensionsController')
     .except(['create', 'show', 'edit'])
-    .as('extensions');
+    .as('extensions')
 
   //Iax Routes
   Route.resource('iaxs', 'IaxController')
     .except(['create', 'show', 'edit'])
-    .as('iaxs');
+    .as('iaxs')
 
   // Queue Routes
   Route.resource('queues', 'QueuesController')
     .except(['create', 'show', 'edit'])
-    .as('queues');
+    .as('queues')
 
   Route.get('/queues/deleted', 'QueuesController.listDeleted').as(
     'queues.listDeleted'
-  );
+  )
 
   Route.post('/queues/:name', 'QueuesController.activate').as(
     'queues.activate'
-  );
+  )
 
   Route.delete('/queues/:name', 'QueuesController.softdelete').as(
     'queues.softdelete'
-  );
+  )
 
   // Queue Members
   Route.get('/queuemembers', 'QueueMembersController.index').as(
     'queuemembers.index'
-  );
+  )
 
   Route.get(
     '/queuemembers/list/:protocol/:endpoint',
     'QueueMembersController.list'
-  ).as('queuemembers.list');
+  ).as('queuemembers.list')
 
   Route.post('/queuemembers', 'QueueMembersController.store').as(
     'queuemembers.store'
-  );
+  )
 
   Route.put(
     '/queuemembers/:protocol/:endpoint',
     'QueueMembersController.update'
-  ).as('queuemembers.update');
+  ).as('queuemembers.update')
 
   Route.delete(
     '/queuemembers/:protocol/:endpoint',
     'QueueMembersController.destroy'
-  ).as('queuemembers.destroy');
-}).prefix('/api');
+  ).as('queuemembers.destroy')
+
+  // Colocar status 404
+  Route.get('*', () => {
+    return { message: 'Not Found' }
+  })
+}).prefix('/api')
