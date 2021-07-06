69ff17e0c0a0f694e56dc806b8fb83d6fdd9efae
georginmatheus <georginmatheus@gmail.com>
2021-07-06 14:23:41 -0300

    test: return 400 if any required field is not provided when trying to register endpoint

---
+++ tests/queue.spec.ts
@@ -0,0 +1,110 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${String(process.env.HOST)}:${String(
+  process.env.PORT
+)}/api`
+
+test.group('Endpoint Controller', () => {
+  test('Should return 400 if id is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        transport: 'any_transport',
+        aors: 'any_aors',
+        auth: 'any_auths',
+        context: 'any_context',
+        mac_address: 'any_mac',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.exists(body)
+  })
+
+  test('Should return 400 if transport is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: 'any_id',
+        aors: 'any_aors',
+        auth: 'any_auths',
+        context: 'any_context',
+        mac_address: 'any_mac',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.exists(body)
+  })
+
+  test('Should return 400 if aors is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: 'any_id',
+        transport: 'any_transport',
+        auth: 'any_auths',
+        context: 'any_context',
+        mac_address: 'any_mac',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.exists(body)
+  })
+
+  test('Should return 400 if auth is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: 'any_id',
+        transport: 'any_transport',
+        aors: 'any_aors',
+        context: 'any_context',
+        mac_address: 'any_mac',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.exists(body)
+  })
+
+  test('Should return 400 if context is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: 'any_id',
+        transport: 'any_transport',
+        aors: 'any_aors',
+        auth: 'any_auth',
+        mac_address: 'any_mac',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.exists(body)
+  })
+
+  test('Should return 400 if mac_adress is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: 'any_id',
+        transport: 'any_transport',
+        aors: 'any_aors',
+        auth: 'any_auth',
+        context: 'any_context',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.exists(body)
+  })
+})
