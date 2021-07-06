4e647cd88b0cc9854709ef07c8022c673b6688ee
georginmatheus <georginmatheus@gmail.com>
2021-07-06 14:42:53 -0300

    test: return 201 if endpoint is created

--- tests/queue.spec.ts
+++ tests/queue.spec.ts
@@ -5,14 +5,14 @@ const BASE_URL = `http://${String(process.env.HOST)}:${String(
   process.env.PORT
 )}/api`
 
-test.group('Endpoint Controller', () => {
+test.group('Endpoint Controller Store', () => {
   test('Should return 400 if id is not provided', async (assert) => {
     const { body } = await supertest(BASE_URL)
       .post('/endpoints')
       .send({
         transport: 'any_transport',
         aors: 'any_aors',
-        auth: 'any_auths',
+        auths: 'any_auths',
         context: 'any_context',
         mac_address: 'any_mac',
       })
@@ -29,7 +29,7 @@ test.group('Endpoint Controller', () => {
       .send({
         id: 'any_id',
         aors: 'any_aors',
-        auth: 'any_auths',
+        auths: 'any_auths',
         context: 'any_context',
         mac_address: 'any_mac',
       })
@@ -46,7 +46,7 @@ test.group('Endpoint Controller', () => {
       .send({
         id: 'any_id',
         transport: 'any_transport',
-        auth: 'any_auths',
+        auths: 'any_auths',
         context: 'any_context',
         mac_address: 'any_mac',
       })
@@ -104,6 +104,24 @@ test.group('Endpoint Controller', () => {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400)
+
+    assert.exists(body)
+  })
+
+  test('Should return 201 if endpoint is created', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: '200',
+        transport: 'transport-udp',
+        aors: '200',
+        auth: '200',
+        context: 'from-internal',
+        mac_address: 'any_mac',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
 
     assert.exists(body)
   })
