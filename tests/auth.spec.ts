71a49619764424595a33fb6aa6e9cd3d49338920
georginmatheus <georginmatheus@gmail.com>
2021-07-07 15:31:40 -0300

    should return 404 if id was not provided

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -214,5 +214,15 @@ test.group('Auth Tests', () => {
         password: 'password',
       })
     })
+
+    test('Should return 404 if id was not provided', async (assert) => {
+      const { body } = await supertest(BASE_URL)
+        .delete('/auths')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(404)
+
+      assert.equal(body.message, 'E_ROUTE_NOT_FOUND: Cannot DELETE:/api/auths')
+    })
   })
 })
