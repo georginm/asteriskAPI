5992386e0d82c957365d1893a3715efe65a8a362
georginmatheus <georginmatheus@gmail.com>
2021-07-07 10:20:25 -0300

    test: add test list, delete and store

--- tests/queue.spec.ts
+++ tests/queue.spec.ts
@@ -5,7 +5,7 @@ const BASE_URL = `http://${String(process.env.HOST)}:${String(
   process.env.PORT
 )}/api`
 
-test.group('Endpoint Controller Store', () => {
+test.group('Endpoint Controller - Store', () => {
   test('Should return 400 if id is not provided', async (assert) => {
     const { body } = await supertest(BASE_URL)
       .post('/endpoints')
@@ -108,6 +108,42 @@ test.group('Endpoint Controller Store', () => {
     assert.exists(body)
   })
 
+  test('Should return 400 if provided aor does not exists', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: '200',
+        transport: 'transport-udp',
+        aors: 'not_exists',
+        auth: '200',
+        context: 'from-internal',
+        mac_address: 'any_macs',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(body.message, 'Aor Not Exists')
+  })
+
+  test('Should return 400 if provided auth does not exists', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: '200',
+        transport: 'transport-udp',
+        aors: '200',
+        auth: 'not_exists',
+        context: 'from-internal',
+        mac_address: 'any_macs',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(body.message, 'Auth Not Exists')
+  })
+
   test('Should return 201 if endpoint is created', async (assert) => {
     const { body } = await supertest(BASE_URL)
       .post('/endpoints')
@@ -117,7 +153,7 @@ test.group('Endpoint Controller Store', () => {
         aors: '200',
         auth: '200',
         context: 'from-internal',
-        mac_address: 'any_mac',
+        mac_address: 'any_macs',
       })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
@@ -125,4 +161,73 @@ test.group('Endpoint Controller Store', () => {
 
     assert.exists(body)
   })
+
+  test('Should return 400 if endpoint id already exists', async (assert) => {
+    const { body } = await supertest(BASE_URL)
+      .post('/endpoints')
+      .send({
+        id: '200',
+        transport: 'transport-udp',
+        aors: '200',
+        auth: '200',
+        context: 'from-internal',
+        mac_address: 'any_macs',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.exists(body)
+  })
 })
+
+test.group('Endpoint Controller - List', () => {
+  test('Should return 200 if valid parameters are provided', async () => {
+    const { body } = await supertest(BASE_URL).get(
+      '/endpoints/list/?context=from-internal'
+    )
+  })
+
+  test('Should return 400 if invalid parameters are provided', async (assert) => {
+    const { body } = await supertest(BASE_URL).get(
+      '/endpoints/list/?context=8574#bdDF'
+    )
+    assert.equal(body.message, 'Endpoints Not Exists')
+  })
+})
+
+test.group('Endpoint Controller - Index', () => {
+  test('Should return 200 if list endpoints', async (assert) => {
+    const body = await supertest(BASE_URL)
+      .get('/endpoints')
+      .set('Accept', 'aplication/json')
+      .expect(200)
+
+    assert.exists(body)
+  })
+})
+
+test.group('Endpoint Controller - Delete', () => {
+  test('Should return 200 if delete endpoints', async (assert) => {
+    const message = await supertest(BASE_URL)
+      .delete('/endpoints/200')
+      .expect(200)
+    assert.equal(message.body.message, 'Endpoint Has Been Deleted')
+  })
+
+  test('Should return 404 if id endpoint is not provided', async (assert) => {
+    const message = await supertest(BASE_URL).delete('/endpoints').expect(404)
+    assert.equal(
+      message.body.message,
+      'E_ROUTE_NOT_FOUND: Cannot DELETE:/api/endpoints'
+    )
+  })
+
+  test('Should return 400 if id endpoint not exists', async (assert) => {
+    const message = await supertest(BASE_URL)
+      .delete('/endpoints/asdae!2@354*&6%')
+      .expect(400)
+    assert.equal(message.body.message, 'Endpoint Not Exists')
+  })
+})
+
