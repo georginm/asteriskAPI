0f62c6e946d7568301e9cc267331a5ad483ac973
georginmatheus <georginmatheus@gmail.com>
2021-07-07 12:56:04 -0300

    test: should return 201 if aor is created

--- tests/aor.spec.ts
+++ tests/aor.spec.ts
@@ -1,4 +1,4 @@
-import test from 'japa'
+import test, { group } from 'japa'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${String(process.env.HOST)}:${String(
@@ -16,7 +16,11 @@ test.group('Aor Test', () => {
     })
   })
 
-  test.group('Aor controller - Store', () => {
+  test.group('Aor controller - Store', (group) => {
+    group.after(async () => {
+      await supertest(BASE_URL).delete('/aors/66666')
+    })
+
     test('Should return 400 if id is not provided', async (asserts) => {
       const { body } = await supertest(BASE_URL)
         .post('/aors')
@@ -31,11 +35,26 @@ test.group('Aor Test', () => {
       asserts.equal(body.message, 'Aor id not provided')
     })
 
+    test('Should return 201 if aor is created', async (asserts) => {
+      const { body } = await supertest(BASE_URL)
+        .post('/aors')
+        .send({
+          id: '66666',
+          contact: 'any_contact',
+          max_contacts: 1,
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+
+      asserts.exists(body)
+    })
+
     test('Should return 400 if id aor already exists', async (asserts) => {
       const { body } = await supertest(BASE_URL)
         .post('/aors')
         .send({
-          id: 200,
+          id: '66666',
         })
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
