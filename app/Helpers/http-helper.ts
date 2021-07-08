2d15a2a59b0973e3e63761aa29deda248e2cbc3c
georginmatheus <georginmatheus@gmail.com>
2021-07-08 11:12:24 -0300

    chore: remove unsed function

--- app/Helpers/http-helper.ts
+++ app/Helpers/http-helper.ts
@@ -1,26 +1,13 @@
 import { ResponseContract } from '@ioc:Adonis/Core/Response'
 
-export const success = (
-  response: ResponseContract,
-  data: object
-): void => {
+export const success = (response: ResponseContract, data: object): void => {
   response.status(200).send(data)
 }
 
-export const created = (
-  response: ResponseContract,
-  data: object
-): void => {
+export const created = (response: ResponseContract, data: object): void => {
   response.status(201).send(data)
 }
 
-export const badRequest = (
-  response: ResponseContract,
-  data: string
-): void => {
+export const badRequest = (response: ResponseContract, data: string): void => {
   response.status(400).send({ message: data })
-}
-
-export const notFound = (response: ResponseContract): void => {
-  response.status(404).send({ message: 'Page not fouund' })
 }
