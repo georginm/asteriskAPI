29f1595eb0952ec186ee9ab0539a1f71511e93a8
georginmatheus <georginmatheus@gmail.com>
2021-07-12 12:50:18 -0300

    test: add before and after functions i each test group

--- tests/endpoint.spec.ts
+++ tests/endpoint.spec.ts
@@ -5,14 +5,51 @@ const BASE_URL = `http://${String(process.env.HOST)}:${String(
   process.env.PORT
 )}/api`
 
+const before = async () => {
+  await supertest(BASE_URL).post('/aors').send({ id: 'any_aor' })
+  await supertest(BASE_URL).post('/auths').send({
+    id: 'any_auth',
+    auth_type: 'userpass',
+    username: 'any_username',
+    password: 'any_password',
+  })
+
+  await supertest(BASE_URL).post('/endpoints').send({
+    id: 'id_exists',
+    transport: 'transport-udp',
+    aors: 'any_aor',
+    auth: 'any_auth',
+    context: 'from-internal',
+    mac_address: 'any_mac_test',
+  })
+}
+
+const after = async () => {
+  await supertest(BASE_URL).delete('/aors/any_aor')
+  await supertest(BASE_URL).delete('/auths/any_auth')
+  await supertest(BASE_URL).delete('/endpoints/id_exists')
+}
+
 test.group('Endpoint Tests', () => {
-  test.group('Endpoint Controller - Store', () => {
+  // #################################################################
+  // ###################### TEST GROUP - STORE ######################
+  // #################################################################
+  test.group('Endpoint Controller - Store', (group) => {
+    group.before(async () => {
+      await before()
+    })
+
+    group.after(async () => {
+      await after()
+      await supertest(BASE_URL).delete('/endpoints/any_id')
+    })
+
     test('Should return 400 if id was not provided', async (assert) => {
       const { body } = await supertest(BASE_URL)
         .post('/endpoints')
         .send({
           transport: 'any_transport',
-          aors: 'any_aors',
+          aors: 'any_aor',
           auths: 'any_auths',
           context: 'any_context',
           mac_address: 'any_mac',
@@ -29,7 +66,7 @@ test.group('Endpoint Tests', () => {
         .post('/endpoints')
         .send({
           id: 'any_id',
-          aors: 'any_aors',
+          aors: 'any_aor',
           auths: 'any_auths',
           context: 'any_context',
           mac_address: 'any_mac',
@@ -64,7 +101,7 @@ test.group('Endpoint Tests', () => {
         .send({
           id: 'any_id',
           transport: 'any_transport',
-          aors: 'any_aors',
+          aors: 'any_aor',
           context: 'any_context',
           mac_address: 'any_mac',
         })
@@ -81,7 +118,7 @@ test.group('Endpoint Tests', () => {
         .send({
           id: 'any_id',
           transport: 'any_transport',
-          aors: 'any_aors',
+          aors: 'any_aor',
           auth: 'any_auth',
           mac_address: 'any_mac',
         })
@@ -92,13 +129,13 @@ test.group('Endpoint Tests', () => {
       assert.exists(body)
     })
 
-    test('Should return 400 if mac_adress was not provided', async (assert) => {
+    test('Should return 400 if mac_address was not provided', async (assert) => {
       const { body } = await supertest(BASE_URL)
         .post('/endpoints')
         .send({
           id: 'any_id',
           transport: 'any_transport',
-          aors: 'any_aors',
+          aors: 'any_aor',
           auth: 'any_auth',
           context: 'any_context',
         })
@@ -113,10 +150,10 @@ test.group('Endpoint Tests', () => {
       const { body } = await supertest(BASE_URL)
         .post('/endpoints')
         .send({
-          id: '200',
+          id: 'any_id',
           transport: 'transport-udp',
           aors: 'not_exists',
-          auth: '200',
+          auth: 'any_aor',
           context: 'from-internal',
           mac_address: 'any_macs',
         })
@@ -131,12 +168,12 @@ test.group('Endpoint Tests', () => {
       const { body } = await supertest(BASE_URL)
         .post('/endpoints')
         .send({
-          id: '200',
+          id: 'any_id',
           transport: 'transport-udp',
-          aors: '200',
+          aors: 'any_aor',
           auth: 'not_exists',
           context: 'from-internal',
-          mac_address: 'any_macs',
+          mac_address: 'any_mac_test',
         })
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
@@ -149,10 +186,10 @@ test.group('Endpoint Tests', () => {
       const { body } = await supertest(BASE_URL)
         .post('/endpoints')
         .send({
-          id: '200',
+          id: 'any_id',
           transport: 'transport-udp',
-          aors: '200',
-          auth: '200',
+          aors: 'any_aor',
+          auth: 'any_auth',
           context: 'from-internal',
           mac_address: 'any_macs',
         })
@@ -167,10 +204,10 @@ test.group('Endpoint Tests', () => {
       const { body } = await supertest(BASE_URL)
         .post('/endpoints')
         .send({
-          id: '200',
+          id: 'id_exists',
           transport: 'transport-udp',
-          aors: '200',
-          auth: '200',
+          aors: 'any_aor',
+          auth: 'any_auth',
           context: 'from-internal',
           mac_address: 'any_macs',
         })
@@ -178,11 +215,22 @@ test.group('Endpoint Tests', () => {
         .expect('Content-Type', /json/)
         .expect(400)
 
-      assert.exists(body)
+      assert.equal(body.message, 'Endpoint Already Exists')
     })
   })
 
-  test.group('Endpoint Controller - List', () => {
+  // #################################################################
+  // ###################### TEST GROUP - LIST ########################
+  // #################################################################
+  test.group('Endpoint Controller - List', (group) => {
+    group.before(async () => {
+      await before()
+    })
+
+    group.after(async () => {
+      await after()
+    })
+
     test('Should return 200 if valid parameters are provided', async (assert) => {
       const { body } = await supertest(BASE_URL)
         .get('/endpoints/list/?context=from-internal')
@@ -210,10 +258,21 @@ test.group('Endpoint Tests', () => {
     })
   })
 
-  test.group('Endpoint Controller - Update', () => {
+  // #################################################################
+  // ###################### TEST GROUP - UPDATE ######################
+  // #################################################################
+  test.group('Endpoint Controller - Update', (group) => {
+    group.before(async () => {
+      await before()
+    })
+
+    group.after(async () => {
+      await after()
+    })
+
     test('Should return 400 if id endpoint not exists', async (assert) => {
       const { body } = await supertest(BASE_URL)
-        .put('/endpoints/asdae!2@354*&6%')
+        .put('/endpoints/id_not_exists')
         .expect(400)
       assert.equal(body.message, 'Endpoint Not Exists')
     })
@@ -225,7 +284,7 @@ test.group('Endpoint Tests', () => {
 
     test('Should return 200 if endpoint has been updated', async (assert) => {
       const { body } = await supertest(BASE_URL)
-        .put('/endpoints/200')
+        .put('/endpoints/id_exists')
         .send({ context: 'any-context' })
         .set('Accept', 'aplication/json')
         .expect(200)
@@ -234,10 +293,21 @@ test.group('Endpoint Tests', () => {
     })
   })
 
-  test.group('Endpoint Controller - Delete', () => {
+  // #################################################################
+  // ###################### TEST GROUP - DELETE ######################
+  // #################################################################
+  test.group('Endpoint Controller - Delete', (group) => {
+    group.before(async () => {
+      await before()
+    })
+
+    group.after(async () => {
+      await after()
+    })
+
     test('Should return 200 if delete endpoints', async (assert) => {
       const { body } = await supertest(BASE_URL)
-        .delete('/endpoints/200')
+        .delete('/endpoints/id_exists')
         .expect(200)
       assert.equal(body.message, 'Endpoint Has Been Deleted')
     })
@@ -254,7 +324,7 @@ test.group('Endpoint Tests', () => {
 
     test('Should return 400 if id endpoint not exists', async (assert) => {
       const { body } = await supertest(BASE_URL)
-        .delete('/endpoints/asdae!2@354*&6%')
+        .delete('/endpoints/id_not_exists')
         .expect(400)
       assert.equal(body.message, 'Endpoint Not Exists')
     })
