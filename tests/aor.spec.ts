538ea03af80cb836ac87ef2834ef8817a44b765b
georginmatheus <georginmatheus@gmail.com>
2021-07-12 12:49:17 -0300

    refactor: change standard style

--- tests/aor.spec.ts
+++ tests/aor.spec.ts
@@ -1,9 +1,7 @@
 import test from 'japa'
 import supertest from 'supertest'
 
-const BASE_URL = `http://${String(process.env.HOST)}:${String(
-  process.env.PORT
-)}/api`
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}/api`
 
 test.group('Aor Test', () => {
   test.group('Aor Controller - Index', () => {
