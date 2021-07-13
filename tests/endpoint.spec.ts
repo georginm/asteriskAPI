24a5070424e6a6d7db17f1cbb59dc3520b636c9f
georginmatheus <georginmatheus@gmail.com>
2021-07-13 16:17:32 -0300

    test: add test to check if codec exists

--- tests/endpoint.spec.ts
+++ tests/endpoint.spec.ts
@@ -297,6 +297,29 @@ test.group('Endpoint Tests', () => {
         'O campo disallow não corresponde com o padrão aceito.'
       )
     })
+
+    test('Should return 400 if an invalid codec was provided', async (assert) => {
+      const { body } = await supertest(BASE_URL)
+        .post('/endpoints')
+        .send({
+          id: 'any',
+          transport: 'udp',
+          aors: 'aors2',
+          auth: 'auth2',
+          context: 'teste',
+          mac_address: '01:23:45:67:89:AE',
+          disallow: 'lsss',
+          allow: 'alaw',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+
+      assert.equal(
+        body.message[0].message,
+        'O campo disallow deve conter um codec válido.'
+      )
+    })
     // ###############################################################
 
     // ########################## ALLOW ##############################
@@ -362,6 +385,29 @@ test.group('Endpoint Tests', () => {
       assert.equal(
         body.message[0].message,
         'O campo allow não corresponde com o padrão aceito.'
+      )
+    })
+
+    test('Should return 400 if an invalid codec was provided', async (assert) => {
+      const { body } = await supertest(BASE_URL)
+        .post('/endpoints')
+        .send({
+          id: 'any',
+          transport: 'udp',
+          aors: 'aors2',
+          auth: 'auth2',
+          context: 'teste',
+          mac_address: '01:23:45:67:89:AE',
+          disallow: 'all',
+          allow: 'alsad',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+
+      assert.equal(
+        body.message[0].message,
+        'O campo allow deve conter um codec válido.'
       )
     })
     // ###############################################################
