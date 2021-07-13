3cae3f0393a1b9cc8b0ebe2f5bee0593536d98ff
georginmatheus <georginmatheus@gmail.com>
2021-07-13 15:50:08 -0300

    test: add tests for contact_deny

--- tests/endpoint.spec.ts
+++ tests/endpoint.spec.ts
@@ -752,7 +752,106 @@ test.group('Endpoint Tests', () => {
     })
     // ###############################################################
 
-    // ############################ PERMIT ###########################
+    // ######################## CONTACT DENY #########################
+    test('Should return 400 if an invalid ip was provided', async (assert) => {
+      const { body } = await supertest(BASE_URL)
+        .post('/endpoints')
+        .send({
+          id: 'test',
+          transport: 'udp',
+          aors: 'aors2',
+          auth: 'auth2',
+          context: 'from-internal',
+          mac_address: '01:23:45:67:89:B4',
+          disallow: 'all',
+          allow: 'alaw',
+          contact_deny: '154.145.142.9999',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+
+      assert.equal(
+        body.message[0].message,
+        'Um dos parâmetros informados no campo contact_deny é invalido'
+      )
+    })
+
+    test('Should return 400 if an invalid mask was provided', async (assert) => {
+      const { body } = await supertest(BASE_URL)
+        .post('/endpoints')
+        .send({
+          id: 'test',
+          transport: 'udp',
+          aors: 'aors2',
+          auth: 'auth2',
+          context: 'from-internal',
+          mac_address: '01:23:45:67:89:B4',
+          disallow: 'all',
+          allow: 'alaw',
+          contact_deny: '154.145.142.999/266.254.215.25',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+
+      assert.equal(
+        body.message[0].message,
+        'Um dos parâmetros informados no campo contact_deny é invalido'
+      )
+    })
+
+    test('Should return 400 if contact_deny exceeds the maximum length', async (assert) => {
+      const { body } = await supertest(BASE_URL)
+        .post('/endpoints')
+        .send({
+          id: 'id_',
+          transport: 'udp',
+          aors: 'aors2',
+          auth: 'auth2',
+          context: 'any_context',
+          mac_address: '01:23:45:67:89:AE',
+          disallow: 'all',
+          allow: 'alaw',
+          contact_deny:
+            '255.64.2.199/145.8.218.54,255.64.2.199/255.255.255.255,255.64.2.199/145.8.218.54,255.64.2.199/255.255.255.255',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+
+      assert.equal(
+        body.message[0].message,
+        'O campo contact_deny deve ser de no máximo 95 caracteres.'
+      )
+    })
+
+    test('Should return 400 if contact_deny is below the minimum length', async (assert) => {
+      const { body } = await supertest(BASE_URL)
+        .post('/endpoints')
+        .send({
+          id: 'any',
+          transport: 'udp',
+          aors: 'aors2',
+          auth: 'auth2',
+          context: 'any_context',
+          mac_address: '01:23:45:67:89:AE',
+          disallow: 'all',
+          allow: 'alaw',
+          contact_deny: '1.1.1.',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+
+      assert.equal(
+        body.message[0].message,
+        'O campo contact_deny deve ser de no mínimo 7 caracteres.'
+      )
+    })
+    // ###############################################################
+
+    // ########################### PERMIT ############################
     test('Should return 400 if an invalid ip was provided', async (assert) => {
       const { body } = await supertest(BASE_URL)
         .post('/endpoints')
