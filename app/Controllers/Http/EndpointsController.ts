981a4dc4eda1b6baebe754daf4eb7c46879d68a9
georginmatheus <georginmatheus@gmail.com>
2021-07-14 13:11:01 -0300

    refactor: remove fields array

--- app/Controllers/Http/EndpointsController.ts
+++ app/Controllers/Http/EndpointsController.ts
@@ -4,40 +4,6 @@ import Endpoint from 'App/Models/Endpoint'
 // import { errorHandle } from 'App/utils/errorHandle'
 import CreateEndpoint from 'App/Validators/CreateEndpointValidator'
 
-// const fields = {
-//   id: 'nome',
-//   transport: 'transporte',
-//   dissallow: 'desabilitar',
-//   allow: 'habilitar',
-//   aors: 'aors',
-//   auth: 'autenticação',
-//   macAddress: 'endereço mac',
-//   deny: 'proibir ip',
-//   permit: 'permitir ip',
-//   contactDeny: 'proibir contato',
-//   contactPermit: 'permitir contato',
-//   callGroup: 'grupo de chamada',
-//   pickupGroup: 'grupo de coleta',
-//   namedCallGroup: 'nome do grupo de chamada',
-//   namedPickupGroup: 'nome do grupo de coleta',
-//   callerId: 'id de chamada',
-//   outboundAuth: 'autenticação de saída',
-//   outboundProxy: 'proxy de saída',
-//   rewriteContact: 'reescrever contato',
-//   rtpSymmetric: 'rtp symmetrico',
-//   forceRPort: 'forçar porta R',
-//   directMedia: 'mídia direta',
-//   disableDirectMediaOnNat: 'desabilitar mídia direta em nat',
-//   iceSupport: 'suporte de gelo',
-//   allowOverlap: 'permitir sobreposição',
-//   rtpTimeOut: 'tempo esgotado rtp',
-//   rtpTimeOutHold: 'tempo de espera rtp',
-//   rtpKeepAlive: 'manter rtp ativo',
-//   timersSessExpires: 'tempo de expiração de sessão',
-//   deviceStateBusyAt: 'estado do dispositivo ocupado em',
-//   dtmfMode: 'modo dtmf',
-// }
-
 export default class EndpointsController {
   public async index({ response }: HttpContextContract) {
     const data = await Endpoint.all()
