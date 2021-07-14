d705600a81afba119600acb8b160e759378e3e68
georginmatheus <georginmatheus@gmail.com>
2021-07-14 11:05:15 -0300

    test: Should return 400 if an invalid rtp_timeout was provided

--- tests/endpoint.spec.ts
+++ tests/endpoint.spec.ts
@@ -1592,6 +1592,32 @@ test.group('Endpoint Tests', () => {
     })
     // ###############################################################
 
+    // ######################## RTP TIMEOUT ########################
+    test('Should return 400 if an invalid rtp_timeout was provided', async (assert) => {
+      const { body } = await supertest(BASE_URL)
+        .post('/endpoints')
+        .send({
+          id: 'id_',
+          transport: 'udp',
+          aors: 'aors2',
+          auth: 'auth2',
+          context: 'any_context',
+          mac_address: '01:23:45:67:89:AE',
+          disallow: 'all',
+          allow: 'alaw',
+          rtp_timeout: 'maybe',
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+
+      assert.equal(
+        body.message[0].message,
+        'O campo rtp_timeout deve ser numérico.'
+      )
+    })
+    // ###############################################################
+
     // ################# ENDPOINT HAS BEEN CREATED ###################
     test('Should return 201 if endpoint has been created', async (assert) => {
       const { body } = await supertest(BASE_URL)
