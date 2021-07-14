1d93c31d9365d54d74439ae5b36bd7e65b6888e5
georginmatheus <georginmatheus@gmail.com>
2021-07-14 13:05:26 -0300

    chore: add endpoint field validators

--- app/Validators/UpdateEndpointValidator.ts
+++ app/Validators/UpdateEndpointValidator.ts
@@ -1,42 +1,132 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import validation from 'App/localization/validation'
 
 export default class UpdateEndpointValidator {
-  constructor (protected ctx: HttpContextContract) {
-  }
+  constructor(protected ctx: HttpContextContract) {}
 
-	/*
-	 * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-	 *
-	 * For example:
-	 * 1. The username must be of data type string. But then also, it should
-	 *    not contain special characters or numbers.
-	 *    ```
-	 *     schema.string({}, [ rules.alpha() ])
-	 *    ```
-	 *
-	 * 2. The email must be of data type string, formatted as a valid
-	 *    email. But also, not used by any other user.
-	 *    ```
-	 *     schema.string({}, [
-	 *       rules.email(),
-	 *       rules.unique({ table: 'users', column: 'email' }),
-	 *     ])
-	 *    ```
-	 */
   public schema = schema.create({
+    id: schema.string({ trim: true }, [
+      rules.maxLength(5),
+      rules.minLength(3),
+      rules.unique({ table: 'ps_endpoints', column: 'id' }),
+    ]),
+
+    transport: schema.enum.optional(['udp', 'tcp', 'tls', 'ws', 'wss']),
+    context: schema.string.optional({ trim: true }, [rules.maxLength(40)]),
+
+    disallow: schema.string.optional({ trim: true }, [
+      rules.maxLength(20),
+      rules.regex(
+        /^(([\w]{3,6})(\s[\w]{3,7})?)(,([\w]{3,6})(\s[\w]{3,7})?){0,12}$/
+      ),
+      rules.codecExists(),
+    ]),
+
+    allow: schema.string.optional({ trim: true }, [
+      rules.maxLength(20),
+      rules.regex(
+        /^(([\w]{3,6}\s?([\w]{3,7})?))(,([\w]{3,6}\s?([\w]{3,7})?)){0,12}$/
+      ),
+      rules.codecExists(),
+    ]),
+
+    aors: schema.string.optional({ trim: true }, [
+      rules.maxLength(5),
+      rules.minLength(3),
+      rules.unique({ table: 'ps_endpoints', column: 'aors' }),
+      rules.exists({ table: 'ps_aors', column: 'id' }),
+    ]),
+
+    auth: schema.string.optional({ trim: true }, [
+      rules.maxLength(5),
+      rules.minLength(3),
+      rules.unique({ table: 'ps_endpoints', column: 'auth' }),
+      rules.exists({ table: 'ps_auths', column: 'id' }),
+    ]),
+
+    mac_address: schema.string.optional({ trim: true }, [
+      rules.maxLength(17),
+      rules.minLength(17),
+      rules.unique({ table: 'ps_endpoints', column: 'mac_address' }),
+      rules.regex(/^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$/),
+    ]),
+
+    deny: schema.string.optional({ trim: true }, [
+      rules.minLength(7),
+      rules.maxLength(95),
+      rules.ipList(),
+    ]),
+
+    permit: schema.string.optional({ trim: true }, [
+      rules.minLength(7),
+      rules.maxLength(95),
+      rules.ipList(),
+    ]),
+
+    contact_deny: schema.string.optional({ trim: true }, [
+      rules.minLength(7),
+      rules.maxLength(95),
+      rules.ipList(),
+    ]),
+
+    contact_permit: schema.string.optional({ trim: true }, [
+      rules.minLength(7),
+      rules.maxLength(95),
+      rules.ipList(),
+    ]),
+
+    call_group: schema.string.optional({ trim: true }, [
+      rules.maxLength(40),
+      rules.callGroupExists(),
+    ]),
+
+    pickup_group: schema.string.optional({ trim: true }, [
+      rules.maxLength(40),
+      rules.callGroupExists(),
+    ]),
+
+    named_call_group: schema.string.optional({ trim: true }, [
+      rules.maxLength(40),
+    ]),
+    named_pickup_group: schema.string.optional({ trim: true }, [
+      rules.maxLength(40),
+    ]),
+
+    callerid: schema.string.optional({ trim: true }, [rules.maxLength(40)]),
+
+    outbound_auth: schema.string.optional({ trim: true }, [
+      rules.maxLength(40),
+    ]),
+
+    outbound_proxy: schema.string.optional({ trim: true }, [
+      rules.maxLength(40),
+    ]),
+
+    rewrite_contact: schema.enum.optional(['yes', 'no'] as const),
+    rtp_symmetric: schema.enum.optional(['yes', 'no'] as const),
+    force_rport: schema.enum.optional(['yes', 'no'] as const),
+    direct_media: schema.enum.optional(['yes', 'no'] as const),
+    t38_udptl: schema.enum.optional(['yes', 'no'] as const),
+    t38_udptl_nat: schema.enum.optional(['yes', 'no'] as const),
+    disable_direct_media_on_nat: schema.enum.optional(['yes', 'no'] as const),
+    ice_support: schema.enum.optional(['yes', 'no'] as const),
+    allow_overlap: schema.enum.optional(['yes', 'no'] as const),
+
+    dtmf_mode: schema.enum.optional([
+      'rfc4733',
+      'inband',
+      'info',
+      'auto',
+      'auto_info',
+    ] as const),
+
+    rtp_timeout: schema.number.optional(),
+    rtp_timeout_hold: schema.number.optional(),
+    rtp_keepalive: schema.number.optional(),
+    timers_sess_expires: schema.number.optional(),
+    device_state_busy_at: schema.number.optional(),
   })
 
-	/**
-	 * Custom messages for validation failures. You can make use of dot notation `(.)`
-	 * for targeting nested fields and array expressions `(*)` for targeting all
-	 * children of an array. For example:
-	 *
-	 * {
-	 *   'profile.username.required': 'Username is required',
-	 *   'scores.*.number': 'Define scores as valid numbers'
-	 * }
-	 *
-	 */
-  public messages = {}
+  public messages = validation
 }
