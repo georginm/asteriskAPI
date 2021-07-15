36a6815ef174870b15447ac5dea9034a396fd545
georginmatheus <georginmatheus@gmail.com>
2021-07-15 13:58:56 -0300

    test: add test to update permit

--- tests/endpoint-update.spec.ts
+++ tests/endpoint-update.spec.ts
@@ -575,4 +575,81 @@ test.group('Endpoint Controller - Update', (group) => {
     assert.equal(body.contact_deny, '255.64.2.199/145.8.218.54')
   })
   // ###############################################################
+
+  // ########################### PERMIT ############################
+  test('Should return 400 if an invalid ip was provided', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .put('/endpoints/id_ex')
+      .send({
+        permit: '154.145.142.9999',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body.message[0].message,
+      'Um dos parâmetros informados no campo permit é invalido'
+    )
+  })
+
+  test('Should return 400 if an invalid mask was provided', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .put('/endpoints/id_ex')
+      .send({
+        permit: '154.145.142.999/266.254.215.25',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body.message[0].message,
+      'Um dos parâmetros informados no campo permit é invalido'
+    )
+  })
+
+  test('Should return 400 if permit exceeds the maximum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .put('/endpoints/id_ex')
+      .send({
+        permit:
+          '255.64.2.199/145.8.218.54,255.64.2.199/255.255.255.255,255.64.2.199/145.8.218.54,255.64.2.199/255.255.255.255',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body.message[0].message,
+      'O campo permit deve ser de no máximo 95 caracteres.'
+    )
+  })
+
+  test('Should return 400 if permit is below the minimum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .put('/endpoints/id_ex')
+      .send({
+        permit: '1.1.1.',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body.message[0].message,
+      'O campo permit deve ser de no mínimo 7 caracteres.'
+    )
+  })
+
+  test('Should return 200 if permit has been updated', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .put('/endpoints/id_ex')
+      .send({ permit: '255.64.2.199/145.8.218.54' })
+      .set('Accept', 'aplication/json')
+      .expect(200)
+
+    assert.equal(body.permit, '255.64.2.199/145.8.218.54')
+  })
+  // ###############################################################
 })
