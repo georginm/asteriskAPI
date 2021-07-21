5fda7a6db30413d3af022a7ded717e2cc696702d
georginmatheus <georginmatheus@gmail.com>
2021-07-21 16:14:02 -0300

    refactor: remove http response helpers from ExtensionsController to use native Adonis response

--- app/Controllers/Http/ExtensionsController.ts
+++ app/Controllers/Http/ExtensionsController.ts
@@ -1,19 +1,18 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { badRequest, created, success } from 'App/Helpers/http-helper'
 import Extension from 'App/Models/Extension'
 
 export default class ExtensionsController {
   public async index({ response }: HttpContextContract) {
     const data = await Extension.query().orderBy('context').orderBy('priority')
-    return success(response, data)
+    return response.ok(data)
   }
 
   public async store({ request, response }: HttpContextContract) {
     try {
       const data = await Extension.create(request.body())
-      return created(response, data)
+      return response.created(data)
     } catch (error) {
-      return badRequest(response, error.message)
+      return response.badRequest(error.message)
     }
   }
 
@@ -23,7 +22,7 @@ export default class ExtensionsController {
     const data = await Extension.find(id)
 
     if (!data) {
-      return badRequest(response, 'Extension Not Exists')
+      return response.badRequest({ message: 'Extension Not Exists' })
     }
 
     await data.delete()
@@ -33,7 +32,7 @@ export default class ExtensionsController {
       ...request.body(),
     })
 
-    return success(response, newData)
+    return response.ok(newData)
   }
 
   public async destroy({ request, response }: HttpContextContract) {
@@ -42,12 +41,12 @@ export default class ExtensionsController {
     const data = await Extension.find(id)
 
     if (!data) {
-      return badRequest(response, 'Extension Not Exists')
+      return response.badRequest({ message: 'Extension Not Exists' })
     }
 
     await data.delete()
 
-    return success(response, {
+    return response.ok({
       message: 'Extension Has Been Deleted',
     })
   }
@@ -56,9 +55,9 @@ export default class ExtensionsController {
     const { id } = request.params()
     const data = await Extension.find(id)
     if (!data) {
-      return badRequest(response, 'Extension Not Exists')
+      return response.badRequest({ message: 'Extension Not Exists' })
     }
 
-    return success(response, data)
+    return response.ok(data)
   }
 }
