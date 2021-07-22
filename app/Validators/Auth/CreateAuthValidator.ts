0166ed2bb11b0bca979d951284a7ebfc2a2dd811
georginmatheus <georginmatheus@gmail.com>
2021-07-22 16:29:55 -0300

    feat: Add CreateAuthValidator

---
+++ app/Validators/Auth/CreateAuthValidator.ts
@@ -0,0 +1,22 @@
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import validation from 'App/localization/validation'
+
+export default class CreateAuthValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    id: schema.string({ trim: true }, [
+      rules.maxLength(5),
+      rules.minLength(5),
+      rules.unique({ table: 'ps_auths', column: 'id' }),
+    ]),
+
+    username: schema.string({ trim: true }, [rules.maxLength(40)]),
+    password: schema.string({ trim: true }, [rules.maxLength(80)]),
+
+    authType: schema.enum(['userpass']),
+  })
+
+  public messages = validation
+}
