d28d03ce9bb98ad43392a0c493e25313f4f7ebe3
georginmatheus <georginmatheus@gmail.com>
2021-07-22 08:42:55 -0300

    refactor: change attribute appData to appdata

--- database/seeders/ExtensionSeeder.ts
+++ database/seeders/ExtensionSeeder.ts
@@ -9,28 +9,28 @@ export default class ExtensionsSeederSeeder extends BaseSeeder {
         exten: '*100',
         priority: 4,
         app: 'hangup',
-        appData: '',
+        appdata: '',
       },
       {
         context: 'from-internal',
         exten: '*100',
         priority: 3,
         app: 'playback',
-        appData: 'hello-world',
+        appdata: 'hello-world',
       },
       {
         context: 'from-internal',
         exten: '*100',
         priority: 2,
         app: 'wait',
-        appData: '1',
+        appdata: '1',
       },
       {
         context: 'from-internal',
         exten: '*100',
         priority: 1,
         app: 'answer',
-        appData: '',
+        appdata: '',
       },
     ])
   }
