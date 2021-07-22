9142bc99476225bb9f17556407d9e51894f4794b
georginmatheus <georginmatheus@gmail.com>
2021-07-22 14:38:43 -0300

    test: add aor list test

---
+++ tests/Aor/aor-list.spec.ts
@@ -0,0 +1,71 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+test.group('Aor controller - List', (group) => {
+  group.after(async () => {
+    await supertest(process.env.BASE_URL).delete('/aors/6666')
+  })
+
+  // ############################## ID ###############################
+  test('Should return 400 if id aor not exists', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .get('/aors/list/?id=not_e')
+      .set('Accept', 'application/json')
+      .expect(400)
+
+    assert.equal(body[0].message, 'O registro de id não existe.')
+  })
+
+  test('Should return 400 if id exceed the maximum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .get('/aors/list/?id=exceeed')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body[0].message,
+      'O campo id deve ser de no máximo 5 caracteres.'
+    )
+  })
+
+  test('Should return 400 if id is below the minimum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .get('/aors/list/?id=id')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body[0].message,
+      'O campo id deve ser de no mínimo 3 caracteres.'
+    )
+  })
+
+  // ########################## CONTACT ##############################
+  test('Should return 400 if contact exceed the maximum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .get(
+        '/aors/list/?contact=exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed,,,,exceed'
+      )
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body[0].message,
+      'O campo contact deve ser de no máximo 255 caracteres.'
+    )
+  })
+
+  // ####################### MAX CONTACTS ############################
+  test('Should return 400 if maxContacts receives a string', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .get('/aors/list/?maxContacts=asdas')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(body[0].message, 'O campo maxContacts deve ser numérico.')
+  })
+})
