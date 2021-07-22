adc6820f81b2002fb5f72699d3702fa36ad635b3
georginmatheus <georginmatheus@gmail.com>
2021-07-22 13:11:42 -0300

    feat: add DeleteAorValidator

--- app/Validators/Aor/DeleteAorValidator.ts
+++ app/Validators/Aor/DeleteAorValidator.ts
@@ -1,11 +1,19 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import validation from 'App/localization/validation'
 
 export default class DeleteAorValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({})
+  public schema = schema.create({
+    params: schema.object().members({
+      id: schema.string({ trim: true }, [
+        rules.maxLength(5),
+        rules.minLength(3),
+        rules.exists({ table: 'ps_aors', column: 'id' }),
+      ]),
+    }),
+  })
 
   public messages = validation
 }
