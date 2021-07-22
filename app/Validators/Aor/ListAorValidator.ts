bd1350d180e8c96856077c61ce17392335427b1e
georginmatheus <georginmatheus@gmail.com>
2021-07-22 14:19:46 -0300

    feat: add Aor List validator

--- app/Validators/Aor/ListAorValidator.ts
+++ app/Validators/Aor/ListAorValidator.ts
@@ -1,11 +1,21 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import validation from 'App/localization/validation'
 
 export default class ListAorValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({})
+  public schema = schema.create({
+    id: schema.string.optional({ trim: true }, [
+      rules.maxLength(5),
+      rules.minLength(3),
+      rules.unique({ table: 'ps_aors', column: 'id' }),
+    ]),
+
+    contact: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
+
+    maxContacts: schema.number.optional(),
+  })
 
   public messages = validation
 }
