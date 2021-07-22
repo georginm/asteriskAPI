5c7123c48b7d2721cbd95a9fc2a555d4424ea70d
georginmatheus <georginmatheus@gmail.com>
2021-07-22 08:56:35 -0300

    test: add aors tests

--- tests/Aor/aor-create.spec.ts
+++ tests/Aor/aor-create.spec.ts
@@ -1,194 +1,49 @@
 import test from 'japa'
 import supertest from 'supertest'
 
-const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}/api`
-
-test.skip('Aor Test', () => {
-  test.group('Aor Controller - Index', () => {
-    test('Sould return 200 if list aors', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .get('/aors')
-        .set('Accept', 'aplication/json')
-        .expect(200)
-      assert.exists(body)
-    })
+test.group('Aor controller - Store', (group) => {
+  group.after(async () => {
+    await supertest(process.env.BASE_URL).delete('/aors/6666')
   })
-
-  test.group('Aor controller - Store', (group) => {
-    group.before(async () => {
-      await supertest(BASE_URL).post('/aors').send({
-        id: '9999999999',
-        max_contacts: 3,
+  test('Should return 400 if id was not provided', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/aors')
+      .send({
         contact: 'any_contact',
+        max_contacts: 1,
       })
-      await supertest(BASE_URL).delete('/aors/6666666666')
-    })
+      .set('Accept', 'application/json')
+      .expect(400)
 
-    group.after(async () => {
-      await supertest(BASE_URL).delete('/aors/6666666666')
-      await supertest(BASE_URL).delete('/aors/9999999999')
-    })
-
-    test('Should return 400 if id was not provided', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .post('/aors')
-        .send({
-          contact: 'any_contact',
-          max_contacts: 1,
-        })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(400)
-
-      assert.equal(body.message, 'aor id not provided')
-    })
-
-    test('Should return 201 if aor has been created', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .post('/aors')
-        .send({
-          id: '6666666666',
-          contact: 'any_contact',
-          max_contacts: 1,
-        })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(201)
-
-      assert.exists(body)
-    })
-
-    test('Should return 400 if id aor already exists', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .post('/aors')
-        .send({
-          id: '9999999999',
-        })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(400)
-
-      assert.equal(body.message, 'aor already exists')
-    })
+    assert.equal(body[0].message, 'O campo id é obrigatório.')
   })
 
-  test.group('Aor Controller - Update', (group) => {
-    group.before(async () => {
-      await supertest(BASE_URL).post('/aors').send({ id: '9999999999' })
-    })
-
-    group.after(async () => {
-      await supertest(BASE_URL).delete('/aors/9999999999')
-    })
-
-    test('Should return 404 if id was not provided', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .put('/aors')
-        .send({
-          max_contacts: 6,
-          contact: 'any_changes',
-        })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(404)
-
-      assert.equal(body.message, 'E_ROUTE_NOT_FOUND: Cannot PUT:/api/aors')
-    })
-
-    test('Should return 400 if id provided not exists', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .put('/aors/&hgy%645s$')
-        .send({
-          max_contacts: 6,
-          contact: 'any_changes',
-        })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(400)
-
-      assert.equal(body.message, 'aor not exists')
-    })
-
-    test('Should return 200 if aor has been updated', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .put('/aors/9999999999')
-        .send({
-          max_contacts: 6,
-          contact: 'any_changes',
-        })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-
-      assert.exists(body)
-    })
-  })
-
-  test.group('Aor Controller - Delete', (group) => {
-    group.before(async () => {
-      await supertest(BASE_URL).post('/aors').send({ id: '9999999999' })
-    })
-
-    group.after(async () => {
-      await supertest(BASE_URL).delete('/aors/9999999999')
-    })
-
-    test('Should return 404 if id was not provided', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .delete('/aors')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(404)
-
-      assert.equal(body.message, 'E_ROUTE_NOT_FOUND: Cannot DELETE:/api/aors')
-    })
-
-    test('Should return 400 if id provided not exists', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .delete('/aors/invalid_id')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(400)
-
-      assert.equal(body.message, 'aor not exists')
-    })
-
-    test('Should return 200 if aor has been deleted', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .delete('/aors/9999999999')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-
-      assert.equal(body.message, 'aor has been deleted')
-    })
-  })
-
-  test.group('Aor Controller - List', (group) => {
-    group.before(async () => {
-      await supertest(BASE_URL).post('/aors').send({
-        id: 'any_id',
+  test('Should return 400 if id aor already exists', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/aors')
+      .send({
+        id: 'aors_',
       })
-    })
+      .set('Accept', 'application/json')
+      .expect(400)
 
-    group.after(async () => {
-      await supertest(BASE_URL).delete('/aors/any_id')
-    })
+    assert.equal(body[0].message, 'O campo id deve ser único.')
+  })
 
-    test('Should return 400 is query params provided are invalid', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .get('/aors/list/?id=invalid_id')
-        .set('Accept', 'aplication/json')
-        .expect(400)
-      assert.exists(body)
-    })
+  test('Should return 201 if aor has been created', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/aors')
+      .send({
+        id: '6666',
+        contact: 'any_contact',
+        maxContacts: 1,
+      })
+      .set('Accept', 'application/json')
+      .expect(201)
 
-    test('Should return 200 is list aors', async (assert) => {
-      const { body } = await supertest(BASE_URL)
-        .get('/aors/list/?id=any_id')
-        .set('Accept', 'aplication/json')
-        .expect(200)
-      assert.exists(body)
-    })
+    console.log(body)
+    assert.equal(body.id, '6666')
+    assert.equal(body.contact, 'any_contact')
+    assert.equal(body.max_contacts, 1)
   })
 })
