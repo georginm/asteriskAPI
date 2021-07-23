45d0014f387166ccbe124ad88fc3dd0204d1b0b8
georginmatheus <georginmatheus@gmail.com>
2021-07-23 10:37:39 -0300

    test: Should return 404 if auth id was not provided

--- tests/Auth/auth-update.spec.ts
+++ tests/Auth/auth-update.spec.ts
@@ -16,4 +16,17 @@ test.group('Auth Controller - Update', () => {
     assert.equal(body[0].message, 'O registro de params.id não existe.')
   })
 
+  test('Should return 404 if auth id was not provided', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .put('/auths')
+      .send({
+        username: 'username',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404)
+
+    assert.equal(body.message, 'E_ROUTE_NOT_FOUND: Cannot PUT:/api/auths')
+  })
+
 })
