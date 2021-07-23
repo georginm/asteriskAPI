6296fe93a9dd58da1ee301c20f2b55d57c6fbe71
georginmatheus <georginmatheus@gmail.com>
2021-07-23 12:27:15 -0300

    chore: make optional fields

--- app/Validators/Auth/ListAuthValidator.ts
+++ app/Validators/Auth/ListAuthValidator.ts
@@ -6,17 +6,16 @@ export default class ListAuthValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    id: schema.string({ trim: true }, [
+    id: schema.string.optional({ trim: true }, [
       rules.maxLength(5),
       rules.minLength(3),
       rules.exists({ table: 'ps_auths', column: 'id' }),
     ]),
 
-    username: schema.string({ trim: true }, [
+    username: schema.string.optional({ trim: true }, [
       rules.maxLength(40),
-      rules.unique({ table: 'ps_auths', column: 'username' }),
+      rules.exists({ table: 'ps_auths', column: 'username' }),
     ]),
-    password: schema.string({ trim: true }, [rules.maxLength(80)]),
   })
 
   public messages = validation
