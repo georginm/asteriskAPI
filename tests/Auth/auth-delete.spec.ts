2c178522ce5a655f65a89ef682ba7bde1189596d
georginmatheus <georginmatheus@gmail.com>
2021-07-23 12:57:36 -0300

    test: add function group before to creating data to test

--- tests/Auth/auth-delete.spec.ts
+++ tests/Auth/auth-delete.spec.ts
@@ -2,4 +2,11 @@ import test from 'japa'
 import supertest from 'supertest'
 
 test.group('Auth Controller - Delete', (group) => {
+  group.before(async () => {
+    await supertest(process.env.BASE_URL).post('/auths').send({
+      id: 'any_',
+      username: 'any',
+      password: 'any',
+    })
+  })
 })
