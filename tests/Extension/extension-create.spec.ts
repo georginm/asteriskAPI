c85396bce7b55d7997af5c730a03ac8966df5f63
georginmatheus <georginmatheus@gmail.com>
2021-07-23 16:12:14 -0300

    test: Should return 400 if context exceeds the maximum length

--- tests/Extension/extension-create.spec.ts
+++ tests/Extension/extension-create.spec.ts
@@ -27,4 +27,24 @@ test.group('Extension Controller - Store', async (group) => {
 
     assert.equal(body[0].message, 'O campo context é obrigatório.')
   })
+
+  test('Should return 400 if context exceeds the maximum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/extensions')
+      .send({
+        context: 'any_test-any_test-any_test-any_test-any_test-',
+        exten: '*100',
+        priority: 1,
+        app: 'answer',
+        appdata: 'a',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body[0].message,
+      'O campo context deve ser de no máximo 40 caracteres.'
+    )
+  })
 })
