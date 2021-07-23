34a6ca781a9f200a54996c9a24df5661d8cb996c
georginmatheus <georginmatheus@gmail.com>
2021-07-23 12:29:12 -0300

    test: Should return 400 if id exceed the maximum length

--- tests/Auth/auth-list.spec.ts
+++ tests/Auth/auth-list.spec.ts
@@ -12,4 +12,17 @@ test.group('Auth Controller - List', () => {
 
     assert.equal(body[0].message, 'O registro de id não existe.')
   })
+
+  test('Should return 400 if id exceed the maximum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .get('/auths/list/?id=exceeeeed')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body[0].message,
+      'O campo id deve ser de no máximo 5 caracteres.'
+    )
+  })
 })
