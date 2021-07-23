f771103a4877a0833c52e4c4f885d54c1321e59b
georginmatheus <georginmatheus@gmail.com>
2021-07-23 08:10:39 -0300

    test: Should return 400 if username was not provided

--- tests/Auth/auth-create.spec.ts
+++ tests/Auth/auth-create.spec.ts
@@ -17,4 +17,18 @@ test.group('Auth Controller - Store', (group) => {
     assert.equal(body[0].message, 'O campo id é obrigatório.')
   })
 
+  test('Should return 400 if username was not provided', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/auths')
+      .send({
+        id: 'any_i',
+        authType: 'userpass',
+        password: 'any_password',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(body[0].message, 'O campo username é obrigatório.')
+  })
 })
