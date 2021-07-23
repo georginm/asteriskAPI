8cb6d5d3e620561860d8f8f610ca58a490c76107
georginmatheus <georginmatheus@gmail.com>
2021-07-23 08:11:08 -0300

    test: Should return 400 if authType was not provided

--- tests/Auth/auth-create.spec.ts
+++ tests/Auth/auth-create.spec.ts
@@ -31,4 +31,19 @@ test.group('Auth Controller - Store', (group) => {
 
     assert.equal(body[0].message, 'O campo username é obrigatório.')
   })
+
+  test('Should return 400 if authType was not provided', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/auths')
+      .send({
+        id: 'any_i',
+        username: 'any_user',
+        password: 'any_password',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(body[0].message, 'O campo authType é obrigatório.')
+  })
 })
