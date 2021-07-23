d5e8705786581f9c7d2b644cacfc30f2f4ca7433
georginmatheus <georginmatheus@gmail.com>
2021-07-23 16:11:18 -0300

    test: add function goup.before to creating data to test

--- tests/Extension/extension-create.spec.ts
+++ tests/Extension/extension-create.spec.ts
@@ -2,4 +2,13 @@ import test from 'japa'
 import supertest from 'supertest'
 
 test.group('Extension Controller - Store', async (group) => {
+  group.before(async () => {
+    await supertest(process.env.BASE_URL).post('/extensions').send({
+      context: 'context',
+      exten: '*100',
+      priority: 1,
+      app: 'answer',
+      appdata: 'a',
+    })
+  })
 })
