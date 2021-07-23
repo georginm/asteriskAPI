a32e2ec34a5f403d1cb749a6014664b76f852efe
georginmatheus <georginmatheus@gmail.com>
2021-07-23 16:12:54 -0300

    test: Should return 400 if exten exceeds the maximum length

--- tests/Extension/extension-create.spec.ts
+++ tests/Extension/extension-create.spec.ts
@@ -65,4 +65,24 @@ test.group('Extension Controller - Store', async (group) => {
     assert.equal(body[0].message, 'O campo exten é obrigatório.')
   })
 
+  test('Should return 400 if exten exceeds the maximum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/extensions')
+      .send({
+        exten: 'any_test-any_test-any_test-any_test-any_test-',
+        context: '*100',
+        priority: 1,
+        app: 'answer',
+        appdata: 'a',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body[0].message,
+      'O campo exten deve ser de no máximo 40 caracteres.'
+    )
+  })
+
 })
