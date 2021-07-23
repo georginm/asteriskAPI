7694ed23026d874648dea1171bc36f31986673bb
georginmatheus <georginmatheus@gmail.com>
2021-07-23 08:08:03 -0300

    test: Should return 400 if id was not provided

---
+++ tests/Auth/auth-create.spec.ts
@@ -0,0 +1,20 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+test.group('Auth Controller - Store', (group) => {
+  test('Should return 400 if id was not provided', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/auths')
+      .send({
+        authType: 'userpass',
+        username: 'any_username',
+        password: 'any_password',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(body[0].message, 'O campo id é obrigatório.')
+  })
+
+})
