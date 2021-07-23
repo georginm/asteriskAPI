98fe4f483382bd2292e6ab76e10222ded4dc9c42
georginmatheus <georginmatheus@gmail.com>
2021-07-23 10:00:55 -0300

    test: remove authType

--- tests/Auth/auth-create.spec.ts
+++ tests/Auth/auth-create.spec.ts
@@ -6,7 +6,6 @@ test.group('Auth Controller - Store', (group) => {
     const { body } = await supertest(process.env.BASE_URL)
       .post('/auths')
       .send({
-        authType: 'userpass',
         username: 'any_username',
         password: 'any_password',
       })
@@ -22,7 +21,6 @@ test.group('Auth Controller - Store', (group) => {
       .post('/auths')
       .send({
         id: 'auth_',
-        authType: 'userpass',
         username: 'anyusername',
         password: 'password',
       })
@@ -38,7 +36,6 @@ test.group('Auth Controller - Store', (group) => {
       .post('/auths')
       .send({
         id: 'id_exceeds',
-        authType: 'userpass',
         username: 'anyusername',
         password: 'password',
       })
@@ -57,7 +54,6 @@ test.group('Auth Controller - Store', (group) => {
       .post('/auths')
       .send({
         id: 'id',
-        authType: 'userpass',
         username: 'anyusername',
         password: 'password',
       })
@@ -90,7 +86,6 @@ test.group('Auth Controller - Store', (group) => {
       .post('/auths')
       .send({
         id: 'any_i',
-        authType: 'userpass',
         password: 'any_password',
       })
       .set('Accept', 'application/json')
@@ -105,7 +100,6 @@ test.group('Auth Controller - Store', (group) => {
       .post('/auths')
       .send({
         id: 'any',
-        authType: 'userpass',
         username: 'any_username',
         password: 'password',
       })
@@ -121,7 +115,6 @@ test.group('Auth Controller - Store', (group) => {
       .post('/auths')
       .send({
         id: 'any_i',
-        authType: 'userpass',
         username: '-anyusername-anyusername-anyusername-anyusername',
         password: 'password',
       })
@@ -135,27 +128,11 @@ test.group('Auth Controller - Store', (group) => {
     )
   })
 
-  test('Should return 400 if authType was not provided', async (assert) => {
-    const { body } = await supertest(process.env.BASE_URL)
-      .post('/auths')
-      .send({
-        id: 'any_i',
-        username: 'any_user',
-        password: 'any_password',
-      })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(400)
-
-    assert.equal(body[0].message, 'O campo authType é obrigatório.')
-  })
-
   test('Should return 400 if password was not provided', async (assert) => {
     const { body } = await supertest(process.env.BASE_URL)
       .post('/auths')
       .send({
         id: 'any_i',
-        authType: 'userpass',
         username: 'any_useryarn',
       })
       .set('Accept', 'application/json')
