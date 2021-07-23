8ae1e6e719a9ba1eca2afc24cc63db25431c4658
georginmatheus <georginmatheus@gmail.com>
2021-07-23 12:28:46 -0300

    test: Should return 400 if auth id provided not exists

--- tests/Auth/auth-list.spec.ts
+++ tests/Auth/auth-list.spec.ts
@@ -2,4 +2,14 @@ import test from 'japa'
 import supertest from 'supertest'
 
 test.group('Auth Controller - List', () => {
+  // ############################### ID ##############################
+  test('Should return 400 if auth id provided not exists', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .get('/auths/list/?id=not_e')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(body[0].message, 'O registro de id não existe.')
+  })
 })
