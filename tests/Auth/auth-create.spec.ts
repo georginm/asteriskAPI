522cf9f698bcbf4afd11895d2f0e0b0ccc4453f0
georginmatheus <georginmatheus@gmail.com>
2021-07-23 08:44:37 -0300

    test: Should return 400 if id exceed the maximum length

--- tests/Auth/auth-create.spec.ts
+++ tests/Auth/auth-create.spec.ts
@@ -115,6 +115,26 @@ test.group('Auth Controller - Store', (group) => {
 
     assert.equal(body[0].message, 'O campo username deve ser único.')
   })
+
+  test('Should return 400 if id exceed the maximum length', async (assert) => {
+    const { body } = await supertest(process.env.BASE_URL)
+      .post('/auths')
+      .send({
+        id: 'any_i',
+        authType: 'userpass',
+        username: '-anyusername-anyusername-anyusername-anyusername',
+        password: 'password',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    assert.equal(
+      body[0].message,
+      'O campo username deve ser de no máximo 40 caracteres.'
+    )
+  })
+
   test('Should return 400 if authType was not provided', async (assert) => {
     const { body } = await supertest(process.env.BASE_URL)
       .post('/auths')
