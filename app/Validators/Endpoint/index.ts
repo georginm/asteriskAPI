6b1e7c7f5822cae6c9cc8f3bbeccd9fa05b0187b
georginmatheus <georginmatheus@gmail.com>
2021-07-26 14:27:30 -0300

    refactor: move the exports validator to a new file

---
+++ app/Validators/Endpoint/index.ts
@@ -0,0 +1,4 @@
+export { CreateEndpointValidator } from './CreateEndpointValidator'
+export { UpdateEndpointValidator } from './UpdateEndpointValidator'
+export { ListEndpointValidator } from './ListEndpointValidator'
+export { DeleteEndpointValidator } from './DeleteEndpointValidator'
