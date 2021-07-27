ff06fc3d1c4ab665d2c29bd4ef30ca58ed4fe068
georginmatheus <georginmatheus@gmail.com>
2021-07-27 12:45:04 -0300

    feat: add update method to EndpointService

--- app/Services/EndpointServices.ts
+++ app/Services/EndpointServices.ts
@@ -19,3 +19,30 @@ export default class EndpointService {
       throw new Exception(error, 500)
     }
   }
+
+  public async update(data): Promise<Endpoint> {
+    await exists('ps_endpoints', 'id', data.id, 'endpoint')
+
+    if (data.auth) await unique('ps_endpoints', 'auth', data.auth)
+    if (data.aors) await unique('ps_endpoints', 'aors', data.aors)
+    if (data.macAddress)
+      await unique('ps_endpoints', 'mac_address', data.macAddress)
+    if (data.auth) await exists('ps_auths', 'id', data.auth, 'auth')
+    if (data.aors) await exists('ps_aors', 'id', data.aors, 'aors')
+
+    try {
+      const item = await Endpoint.find(data.id)
+
+      if (!item) {
+        throw new Exception('Internal Server Error', 500)
+      }
+
+      item.merge(data)
+      await item.save()
+
+      return data
+    } catch (error) {
+      throw new Exception(error, 500)
+    }
+  }
+}
