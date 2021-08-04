a43a252cda29e7093338db86aebce95828575440
georginmatheus <georginmatheus@gmail.com>
2021-08-04 10:35:46 -0300

    refactor: remove param from assert

--- tests/Aor/aor-delete.spec.ts
+++ tests/Aor/aor-delete.spec.ts
@@ -24,7 +24,7 @@ test.group('Aor Controller - Delete', (group) => {
       .put('/aors/not_e')
       .set('Accept', 'application/json')
       .expect(400)
-    assert.equal(body[0].message, 'O registro de params.id não existe.')
+    assert.equal(body.message, 'O registro de id não existe.')
   })
 
   test('Should return 400 if id exceed the maximum length', async (assert) => {
