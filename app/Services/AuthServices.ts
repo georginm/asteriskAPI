cbf41cd106ce82550636158054ae8c6018ebf56c
georginmatheus <georginmatheus@gmail.com>
2021-08-05 17:07:50 -0300

    feat: add create method

--- app/Services/AuthServices.ts
+++ app/Services/AuthServices.ts
@@ -1,2 +1,16 @@
+import { Exception } from '@adonisjs/core/build/standalone'
+import Auth from 'App/Models/Auth'
+import { unique } from 'App/utils/unique'
+
 export default class AuthServices {
+  public async create(data): Promise<Auth> {
+    await unique('ps_auths', 'id', data.id, 'id')
+    await unique('ps_auths', 'username', data.username, 'username')
+
+    try {
+      return await Auth.create(data)
+    } catch (error) {
+      throw new Exception(error, 500)
+    }
+  }
 }
