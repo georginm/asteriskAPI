5e08475de8923d8e1ce256a042e1e64f0b66bd0f
georginmatheus <georginmatheus@gmail.com>
2021-08-05 17:08:21 -0300

    feat: add update method

--- app/Services/AuthServices.ts
+++ app/Services/AuthServices.ts
@@ -1,5 +1,6 @@
 import { Exception } from '@adonisjs/core/build/standalone'
 import Auth from 'App/Models/Auth'
+import { exists } from 'App/utils/exists'
 import { unique } from 'App/utils/unique'
 
 export default class AuthServices {
@@ -13,4 +14,25 @@ export default class AuthServices {
       throw new Exception(error, 500)
     }
   }
+
+  public async update(data, id: string): Promise<Auth> {
+    await exists('ps_auths', 'id', id, 'id')
+    await unique('ps_auths', 'username', data.username, 'username')
+
+    try {
+      const item = await Auth.find(id)
+
+      if (!item) {
+        throw new Exception('Internal Server Error', 500)
+      }
+
+      item.merge(data)
+      await item.save()
+
+      return item
+    } catch (error) {
+      throw new Exception(error, 500)
+    }
+  }
+
 }
