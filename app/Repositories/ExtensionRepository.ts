7b84ae0b27c4dcef4782a3b0c1715e3cfcc2843a
georginm <georginmatheus@gmail.com>
2021-08-13 10:59:32 -0300

    feat: add methot to verify relationships

--- app/Repositories/ExtensionRepository.ts
+++ app/Repositories/ExtensionRepository.ts
@@ -1,2 +1,27 @@
 import Extension from 'App/Models/Extension'
+import { Exception } from '@adonisjs/core/build/standalone'
+
 export default class ExtensionRepository extends Extension {
+  /**
+   * @param priority
+   * @param context
+   * @param exten
+   */
+  public static async uniquePerExtension(
+    priority: string,
+    context: string,
+    exten: string
+  ): Promise<void> {
+    const result = await Extension.query()
+      .where('context', context)
+      .where('exten', exten)
+      .where('priority', priority)
+
+    if (result.length)
+      throw new Exception(
+        'Os campos context, exten e priority devem ser únicos por extension.',
+        400
+      )
+  }
+
+}
