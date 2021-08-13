40462d0fb6834d3eb971e27b5af471fac4898254
georginm <georginmatheus@gmail.com>
2021-08-13 11:05:27 -0300

    feat: add method create

--- app/Services/ExtensionService.ts
+++ app/Services/ExtensionService.ts
@@ -1,3 +1,19 @@
+import { Exception } from '@adonisjs/core/build/standalone'
 import ExtensionRepository from 'App/Repositories/ExtensionRepository'
+
 export default class ExtensionService {
+  public async create(data): Promise<ExtensionRepository> {
+    await ExtensionRepository.uniquePerExtension(
+      data.priority,
+      data.context,
+      data.exten
+    )
+
+    try {
+      return await ExtensionRepository.create(data)
+    } catch (error) {
+      throw new Exception(error, 500)
+    }
+  }
+
 }
