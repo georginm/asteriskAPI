75d2310428fed91bae9cd99c8adfead4130674c5
georginm <georginmatheus@gmail.com>
2021-08-13 11:07:37 -0300

    feat: add DeleteExtensionValidator

---
+++ app/Validators/Extension/DeleteExtensionValidator.ts
@@ -0,0 +1,17 @@
+import { schema } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import validation from 'App/localization/validation'
+
+class DeleteExtensionValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    params: schema.object().members({
+      id: schema.number(),
+    }),
+  })
+
+  public messages = validation
+}
+
+export { DeleteExtensionValidator }
