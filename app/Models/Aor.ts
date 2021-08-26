d93a7132a56c127a0e237ebb3334d7719cf70587
georginm <georginmatheus@gmail.com>
2021-08-26 09:34:52 -0300

    chore: add remaining fields from table to model

--- app/Models/Aor.ts
+++ app/Models/Aor.ts
@@ -9,6 +9,33 @@ export default class Aor extends BaseModel {
   @column()
   public contact: string
 
+  @column({ columnName: 'outbound_proxy' })
+  public outboundProxy: string // varchar 40
+
   @column({ columnName: 'max_contacts' })
   public maxContacts: number
+
+  @column({ columnName: 'minimum_expiration' })
+  public minimumExpiration: number
+
+  @column({ columnName: 'maximum_expiration' })
+  public maximumExpiration: number
+
+  @column({ columnName: 'qualify_timeout' })
+  public qualify_timeout: number // float8
+
+  @column({ columnName: 'qualify_frequency' })
+  public qualifyFrequency: number // int
+
+  @column({ columnName: 'default_expiration' })
+  public defaultExpiration: number
+
+  @column({ columnName: 'support_path' })
+  public supportPath: string // yesno_values
+
+  @column({ columnName: 'remove_existing' })
+  public removeExisting: string // yesno_values
+
+  @column({ columnName: 'authenticate_qualify' })
+  public authenticateQualify: string // yesno_values
 }
--- app/Validators/Aor/CreateAorValidator.ts
+++ app/Validators/Aor/CreateAorValidator.ts
@@ -12,8 +12,20 @@ class CreateAorValidator {
     ]),
 
     contact: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
+    outboundProxy: schema.string.optional({ trim: true }, [
+      rules.maxLength(40),
+    ]),
+
+    supportPath: schema.enum.optional(['yes', 'no'] as const),
+    removeExisting: schema.enum.optional(['yes', 'no'] as const),
+    authenticateQualify: schema.enum.optional(['yes', 'no'] as const),
 
     maxContacts: schema.number.optional(),
+    maximumExpiration: schema.number.optional(),
+    minimumExpiration: schema.number.optional(),
+    qualify_timeout: schema.number.optional(),
+    qualifyFrequency: schema.number.optional(),
+    defaultExpiration: schema.number.optional(),
   })
 
   public messages = validation
