20decc6bede14415751e446d00966d6f7534dc70
georginm <georginmatheus@gmail.com>
2021-09-16 14:32:00 -0300

    chore: add RegistrationService

---
+++ app/Services/RegistrationService.ts
@@ -0,0 +1,52 @@
+import BadRequestException from 'App/Exceptions/BadRequestException'
+import InternalServerErrorException from 'App/Exceptions/InternalServerErrorException'
+import RegistrationRepository from 'App/Repositories/RegistrationRepository'
+import { destroy, exists, unique } from 'App/utils/database'
+
+export default class RegistrationService {
+  public async index(): Promise<RegistrationRepository[]> {
+    const data = await RegistrationRepository.index()
+
+    if (!data.length) throw new BadRequestException('Registration not Exists')
+
+    return data
+  }
+
+  public async show(data): Promise<RegistrationRepository[]> {
+    const item = await RegistrationRepository.show(data)
+
+    if (!item.length) throw new BadRequestException('Registration not Exists')
+
+    return item
+  }
+
+  public async create(data): Promise<RegistrationRepository> {
+    await unique(RegistrationRepository.table, 'id', data.id)
+    await unique(RegistrationRepository.table, 'endpoint', data.endpoint)
+
+    try {
+      return await RegistrationRepository.create(data)
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  public async destroy(id) {
+    return await destroy(RegistrationRepository.table, 'id', id)
+  }
+
+  public async update(data, id): Promise<RegistrationRepository> {
+    await exists(RegistrationRepository.table, 'id', data.id)
+
+    if (data.endpoint)
+      await unique(RegistrationRepository.table, 'endpoint', data.endpoint)
+
+    try {
+      return await (await RegistrationRepository.findOrFail(id))
+        .merge(data)
+        .save()
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+}
