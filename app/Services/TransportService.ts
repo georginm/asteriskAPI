e130365396f36c923e2a0d0e22193d492542d5e7
georginm <georginmatheus@gmail.com>
2021-09-16 11:19:47 -0300

    chore: add create method

--- app/Services/TransportService.ts
+++ app/Services/TransportService.ts
@@ -1,2 +1,15 @@
+import InternalServerErrorException from 'App/Exceptions/InternalServerErrorException'
+import TransportRepository from 'App/Repositories/TransportRepository'
+import { destroy, exists, unique } from 'App/utils/database'
+
 export default class TransportService {
+  public async create(data): Promise<TransportRepository> {
+    unique(TransportRepository.table, 'id', data.id, 'id')
+
+    try {
+      return await TransportRepository.create(data)
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
 }
