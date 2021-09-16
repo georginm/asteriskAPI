f77b1ddbae54e379bdb5053e3312718557588ec8
georginm <georginmatheus@gmail.com>
2021-09-16 14:33:02 -0300

    chore: add index method

--- app/Controllers/Http/RegistrationsController.ts
+++ app/Controllers/Http/RegistrationsController.ts
@@ -1,7 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import RegistrationService from 'App/Services/RegistrationService'
 
 export default class RegistrationsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ response }: HttpContextContract) {
+    const data = await new RegistrationService().index()
+
+    return response.ok(data)
+  }
 
   public async create({}: HttpContextContract) {}
 
