d1db6b65581226d778692497a443648a6d71aa5a
georginm <georginmatheus@gmail.com>
2021-09-16 10:55:07 -0300

    chore: add new fields

--- app/Models/Registration.ts
+++ app/Models/Registration.ts
@@ -1,14 +1,16 @@
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Registration extends BaseModel {
+  public static table = 'ps_registrations'
+
   @column()
   public id: string
 
+  @column({ columnName: 'auth_rejection_permanent' })
+  public authRejectionPermanent: string
+
   @column({ columnName: 'client_uri' })
   public clientUri: string
-
-  @column({ columnName: 'server_uri' })
-  public serverUri: string
 
   @column({ columnName: 'contact_user' })
   public contactUser: string
@@ -16,15 +18,42 @@ export default class Registration extends BaseModel {
   @column()
   public expiration: number
 
+  @column({ columnName: 'max_retries' })
+  public maxRetries: number
+
   @column({ columnName: 'outbound_auth' })
   public outboundAuth: string
 
   @column({ columnName: 'outbound_proxy' })
   public outboundProxy: string
 
-  @column()
-  public endpoint: string
+  @column({ columnName: 'retry_interval' })
+  public retryInterval: number
+
+  @column({ columnName: 'forbidden_retry_interval' })
+  public forbiddenRetryInterval: number
+
+  @column({ columnName: 'server_uri' })
+  public serverUri: string
 
   @column()
   public transport: string
+
+  @column({ columnName: 'support_path' })
+  public supportPath: string
+
+  @column({ columnName: 'fatal_retry_interval' })
+  public fatalRetryInterval: number
+
+  @column()
+  public line: string
+
+  @column()
+  public endpoint: string
+
+  @column({ columnName: 'support_outbound' })
+  public supportOutbound: string
+
+  @column({ columnName: 'contact_header_params' })
+  public contactHeaderParams: string
 }
