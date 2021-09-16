810f6f2e2dfd40acac26ca90694c9719985c4f55
georginm <georginmatheus@gmail.com>
2021-09-16 11:14:43 -0300

    feat: add index method

--- app/Repositories/TransportRepository.ts
+++ app/Repositories/TransportRepository.ts
@@ -1,4 +1,12 @@
+import InternalServerErrorException from 'App/Exceptions/InternalServerErrorException'
 import Transport from 'App/Models/Transport'
 
 export default class TransportRepository extends Transport {
+  public static async index(): Promise<Transport[]> {
+    try {
+      return await Transport.all()
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
 }
