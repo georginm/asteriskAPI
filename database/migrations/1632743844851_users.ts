7d34db39791e56c34d6a0bfea38c6886b576137a
georginm <georginmatheus@gmail.com>
2021-09-28 09:57:37 -0300

    chore: add cpf and fullname to migrations

--- database/migrations/1632743844851_users.ts
+++ database/migrations/1632743844851_users.ts
@@ -7,6 +7,8 @@ export default class UsersSchema extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('email', 255).notNullable()
+      table.string('cpf', 14).notNullable()
+      table.string('fullname', 100).notNullable()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
       table.timestamp('created_at', { useTz: true }).notNullable()
