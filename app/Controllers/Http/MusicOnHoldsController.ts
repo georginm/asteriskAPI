a34700c9ca3bab95d9e8890dab091b247e8c7551
georginm <georginmatheus@gmail.com>
2021-10-04 16:31:36 -0300

    feat: add MusicOnHoldController

---
+++ app/Controllers/Http/MusicOnHoldsController.ts
@@ -0,0 +1,47 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import MusicOnHoldService from 'App/Services/MusicOnHoldService'
+import {
+  CreateMusicOnHoldValidator,
+  UpdateMusicOnHoldValidator,
+  ListMusicOnHoldValidator,
+  DeleteMusicOnHoldValidator,
+} from 'App/Validators/MusicOnHold'
+
+export default class MusicOnHoldsController {
+  public async index({ response }: HttpContextContract) {
+    const data = await new MusicOnHoldService().index()
+    return response.ok(data)
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    await request.validate(CreateMusicOnHoldValidator)
+
+    const data = await new MusicOnHoldService().create(request.body())
+    return response.created(data)
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    await request.validate(UpdateMusicOnHoldValidator)
+
+    const { name } = request.params()
+    const data = await new MusicOnHoldService().update(request.body(), name)
+
+    return response.ok(data)
+  }
+
+  public async destroy({ request, response }: HttpContextContract) {
+    await request.validate(DeleteMusicOnHoldValidator)
+
+    await new MusicOnHoldService().delete(request.params().name)
+
+    return response.ok({ message: 'MusicOnHold Has Been Deleted.' })
+  }
+
+  public async show({ request, response }: HttpContextContract) {
+    await request.validate(ListMusicOnHoldValidator)
+
+    const data = await new MusicOnHoldService().show(request.params().data)
+
+    return response.ok(data)
+  }
+}
