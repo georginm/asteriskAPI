36fc34c268e4a9a7ef741ba00b1b84c012303adf
georginm <georginmatheus@gmail.com>
2021-10-04 16:30:20 -0300

    feat: add MusicOnHoldRepository

--- app/Repositories/MusicOnHoldRepository.ts
+++ app/Repositories/MusicOnHoldRepository.ts
@@ -1,3 +1,27 @@
+import InternalServerErrorException from 'App/Exceptions/InternalServerErrorException'
 import MusicOnHold from 'App/Models/MusicOnHold'
 
-export default class MusicOnHoldRepository extends MusicOnHold {}
+export default class MusicOnHoldRepository extends MusicOnHold {
+  public static async index(): Promise<MusicOnHold[]> {
+    try {
+      return await MusicOnHold.query()
+        .select('name', 'directory', 'sort')
+        .paginate(1, 20)
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  public static async show(data): Promise<MusicOnHold[]> {
+    try {
+      return await MusicOnHold.query()
+        .select('name', 'directory', 'sort')
+        .where('name', data)
+        .orWhere('directory', 'like', `%${data}%`)
+        .orWhere('sort', data)
+        .paginate(1, 20)
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+}
