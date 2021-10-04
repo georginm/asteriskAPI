94313f9ffc140f942849d48adabc23085067d812
georginm <georginmatheus@gmail.com>
2021-10-04 16:26:06 -0300

    fix: correct id type

--- database/migrations/1632743844851_users.ts
+++ database/migrations/1632743844851_users.ts
@@ -5,7 +5,7 @@ export default class UsersSchema extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.string('id').primary()
       table.string('email', 255).notNullable()
       table.string('cpf', 14).notNullable()
       table.string('fullname', 100).notNullable()
