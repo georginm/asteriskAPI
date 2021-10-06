f1cb156ca3e055c377467072d67ee59cbfaa48ea
georginm <georginmatheus@gmail.com>
2021-10-06 08:38:33 -0300

    feat: create users repository

--- app/Repositories/UserRepository.ts
+++ app/Repositories/UserRepository.ts
@@ -1,3 +1,32 @@
+import InternalServerErrorException from 'App/Exceptions/InternalServerErrorException'
 import User from 'App/Models/User'
 
-export default class UserRepository extends User {}
+export default class UserRepository extends User {
+  public static async index(page: number, limit: number): Promise<User[]> {
+    try {
+      return await User.query()
+        .select('id', 'fullname', 'email')
+        .paginate(page, limit)
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  public static async show(
+    data: string,
+    page: number,
+    limit: number
+  ): Promise<User[]> {
+    try {
+      return await User.query()
+        .select('id', 'fullname', 'email')
+        .where('id', data)
+        .orWhere('fullname', 'like', `%${data}%`)
+        .orWhere('cpf', 'like', `%${data}%`)
+        .orWhere('email', 'like', `%${data}%`)
+        .paginate(page, limit)
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+}
