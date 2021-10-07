b8cb198b52c0e44592e64becfe86a511ba715f4d
georginm <georginmatheus@gmail.com>
2021-10-07 16:35:59 -0300

    chore: edit routes

--- start/routes.ts
+++ start/routes.ts
@@ -3,96 +3,53 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   // Endpoint Routes
   Route.resource('/endpoints', 'EndpointsController')
-    .except(['create', 'show', 'edit'])
+    .except(['create', 'edit'])
     .as('endpoint')
 
-  Route.get('/endpoints/show/:data', 'EndpointsController.show').as(
-    'endpoint.show'
-  )
-
   Route.resource('/branches', 'BranchesController')
-    .except(['create', 'show', 'edit'])
+    .except(['create', 'edit'])
     .as('branches')
-
-  Route.get('/branches/show/:data', 'BranchesController.show').as('branch.show')
 
   // Auth Routes
   Route.resource('auths', 'AuthsController')
-    .except(['create', 'show', 'edit'])
+    .except(['create', 'edit'])
     .as('auths')
 
-  Route.get('/auths/show/:data', 'AuthsController.show').as('auth.show')
-
   // Aor Routes
-  Route.resource('aors', 'AorsController')
-    .except(['create', 'show', 'edit'])
-    .as('aors')
-
-  Route.get('/aors/show/:data', 'AorsController.show').as('aors.show')
+  Route.resource('aors', 'AorsController').except(['create', 'edit']).as('aors')
 
   // Extension Routes
   Route.resource('extensions', 'ExtensionsController')
-    .except(['create', 'show', 'edit'])
+    .except(['create', 'edit'])
     .as('extensions')
 
-  Route.get('/extensions/show/:data', 'ExtensionsController.show').as(
-    'extension.show'
-  )
-
   //Iax Routes
-  Route.resource('iaxs', 'IaxsController')
-    .except(['create', 'show', 'edit'])
-    .as('iaxs')
-
-  Route.get('/iaxs/show/:data', 'IaxsController.show').as('iax.show')
+  Route.resource('iaxs', 'IaxsController').except(['create', 'edit']).as('iaxs')
 
   // Queue Routes
-  Route.group(() => {
-    Route.resource('', 'QueuesController').only(['index', 'store']).as('queues')
-
-    Route.put('/:name', 'QueuesController.update').as('queue.update')
-
-    Route.delete('/delete/:name', 'QueuesController.destroy').as('queue.delete')
-
-    Route.get('/show/:data', 'QueuesController.show').as('queues.show')
-  }).prefix('/queues')
+  Route.resource('/queues', 'QueuesController')
+    .except(['create', 'edit'])
+    .as('queues')
 
   // Queue Members
-  Route.group(() => {
-    Route.get('/', 'QueueMembersController.index').as('queuemembers.index')
-
-    Route.get('show/:data', 'QueueMembersController.show').as(
-      'queuemembers.show'
-    )
-
-    Route.post('', 'QueueMembersController.store').as('queuemembers.store')
-
-    Route.delete('/:interface', 'QueueMembersController.destroy').as(
-      'queuemembers.destroy'
-    )
-
-    Route.put('/:interface', 'QueueMembersController.update').as(
-      'queuemembers.update'
-    )
-  }).prefix('/queuemembers')
+  Route.resource('/queuemembers', 'QueueMembersController')
+    .except(['edit', 'create'])
+    .as('queuemembers')
 
   // Transport Routes
   Route.resource('/transports', 'TransportsController')
-    .except(['create', 'show', 'edit'])
+    .except(['create', 'edit'])
     .as('transport')
 
-  Route.get('/transports/show/:data', 'TransportsController.show').as(
-    'transport.show'
-  )
+  // MusiconHold Routes
+  Route.resource('/musiconholds', 'MusicOnHoldsController')
+    .except(['create', 'edit'])
+    .as('musiconhold')
 
   // Registration Routes
   Route.resource('/registrations', 'RegistrationsController')
-    .except(['create', 'show', 'edit'])
+    .except(['create', 'edit'])
     .as('registration')
-
-  Route.get('/registrations/show/:data', 'RegistrationsController.show').as(
-    'registration.show'
-  )
 
   // Users
   Route.resource('/users', 'UsersController')
