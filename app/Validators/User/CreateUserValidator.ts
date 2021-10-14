4d5385495b0f32d8494c8331f6a8669f517f26a9
georginm <georginmatheus@gmail.com>
2021-10-14 14:01:57 -0300

    chore: add minlength and max length

--- app/Validators/User/CreateUserValidator.ts
+++ app/Validators/User/CreateUserValidator.ts
@@ -6,12 +6,19 @@ export default class CreateUserValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    email: schema.string({ trim: true }, [rules.email()]),
+    email: schema.string({ trim: true }, [
+      rules.email(),
+      rules.minLength(9),
+      rules.maxLength(255),
+    ]),
     cpf: schema.string({ trim: true }, [
       rules.regex(/(\d{3})(.)(\d{3})(.)(\d{3})(-)(\d{2}))/),
     ]),
     fullname: schema.string({ trim: true }),
-    password: schema.string({ trim: true }),
+    password: schema.string({ trim: true }, [
+      rules.minLength(6),
+      rules.maxLength(180),
+    ]),
   })
 
   public messages = validation
